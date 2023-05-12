4f77461a9613b3c9904379b28ace22aa25ac6635
MSI\masa_ <masarito0205@gmail.com>
2023-05-12 10:18:10 +0900

    第２章の練習問題を提出

---
+++ ControlCheck/ControlCheck/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelCheckBox.Text = "チェックボックス" + checkBox1.Checked;
+            labelRadioButton1.Text = "ラジオボタン１：" + radioButton1.Checked;
+            labelRadioButton2.Text = "ラジオボタン２：" + radioButton2.Checked;
+            labelNumericUpDown.Text = "数値：" + numericUpDown1.Value;
+        }
+        private void labelCheckBox_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void labelRadioButton1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void labelRadioButton2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void labelNumericUpDown_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            labelCheckBox.Text = "チェックボックス" + checkBox1.Checked;
+        }
+
+        private void radioButton1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            labelRadioButton1.Text = "ラジオボタン１：" + radioButton1.Checked;
+        }
+
+        private void radioButton2_CheckedChanged_1(object sender, EventArgs e)
+        {
+            labelRadioButton2.Text = "ラジオボタン２：" + radioButton2.Checked;
+        }
+
+        private void numericUpDown1_ValueChanged_1(object sender, EventArgs e)
+        {
+            labelNumericUpDown.Text = "数値：" + numericUpDown1.Value;
+        }
+    }
+}

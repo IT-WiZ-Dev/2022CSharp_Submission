a373db2a60fb2de7985f8f2974a03fdbf8b59522
MSI\81905 <zeal22410204@fsgcl.jp>
2023-05-12 10:18:08 +0900

    第2章の「問題を提出しました

---
+++ ControlCheck - コピー/ControlCheck/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            labelCheckBox.Text = "チェックボックス:" + checkBox1.Checked;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            labelRadioButton1.Text = "ラジオボタン1:" + radioButton1.Checked;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            labelRadioBoxButton2.Text = "ラジオボタン2:" + radioButton2.Checked;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            labelNumelicUpDown.Text = "数値:" + numericUpDown2.Value;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelCheckBox.Text = "チェックボックス:" + checkBox1.Checked;
+            labelNumelicUpDown.Text = "数値:" + numericUpDown2.Value;
+            labelRadioBoxButton2.Text = "ラジオボタン2:" + radioButton2.Checked;
+            labelRadioButton1.Text = "ラジオボタン1:" + radioButton1.Checked;
+        }
+    }
+}
---
+++ ControlCheck/ControlCheck/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace ControlCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelCheckBox = new System.Windows.Forms.Label();
+            this.labelRadioButton1 = new System.Windows.Forms.Label();
+            this.labelRadioBoxButton2 = new System.Windows.Forms.Label();
+            this.labelNumelicUpDown = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(30, 30);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 19);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "チェックボックス";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(30, 82);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(104, 19);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "ラジオボタン１";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(30, 135);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(113, 19);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "ラジオボックス２";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(30, 201);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(142, 22);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ニューメリックアップダウン";
+            // 
+            // labelCheckBox
+            // 
+            this.labelCheckBox.AutoSize = true;
+            this.labelCheckBox.Location = new System.Drawing.Point(236, 30);
+            this.labelCheckBox.Name = "labelCheckBox";
+            this.labelCheckBox.Size = new System.Drawing.Size(43, 15);
+            this.labelCheckBox.TabIndex = 7;
+            this.labelCheckBox.Text = "label1";
+            this.labelCheckBox.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // labelRadioButton1
+            // 
+            this.labelRadioButton1.AutoSize = true;
+            this.labelRadioButton1.Location = new System.Drawing.Point(236, 82);
+            this.labelRadioButton1.Name = "labelRadioButton1";
+            this.labelRadioButton1.Size = new System.Drawing.Size(43, 15);
+            this.labelRadioButton1.TabIndex = 8;
+            this.labelRadioButton1.Text = "label2";
+            this.labelRadioButton1.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // labelRadioBoxButton2
+            // 
+            this.labelRadioBoxButton2.AutoSize = true;
+            this.labelRadioBoxButton2.Location = new System.Drawing.Point(236, 135);
+            this.labelRadioBoxButton2.Name = "labelRadioBoxButton2";
+            this.labelRadioBoxButton2.Size = new System.Drawing.Size(43, 15);
+            this.labelRadioBoxButton2.TabIndex = 9;
+            this.labelRadioBoxButton2.Text = "label3";
+            this.labelRadioBoxButton2.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // labelNumelicUpDown
+            // 
+            this.labelNumelicUpDown.AutoSize = true;
+            this.labelNumelicUpDown.Location = new System.Drawing.Point(236, 201);
+            this.labelNumelicUpDown.Name = "labelNumelicUpDown";
+            this.labelNumelicUpDown.Size = new System.Drawing.Size(43, 15);
+            this.labelNumelicUpDown.TabIndex = 10;
+            this.labelNumelicUpDown.Text = "label4";
+            this.labelNumelicUpDown.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 283);
+            this.Controls.Add(this.labelNumelicUpDown);
+            this.Controls.Add(this.labelRadioBoxButton2);
+            this.Controls.Add(this.labelRadioButton1);
+            this.Controls.Add(this.labelCheckBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelCheckBox;
+        private System.Windows.Forms.Label labelRadioButton1;
+        private System.Windows.Forms.Label labelRadioBoxButton2;
+        private System.Windows.Forms.Label labelNumelicUpDown;
+    }
+}
+

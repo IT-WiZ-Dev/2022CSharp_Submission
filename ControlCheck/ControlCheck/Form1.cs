f1f60af32a2ea2a59a1ff30d85e5f42e7a11d105
Wiz22Konno <emailnoreply85@gmail.com>
2023-05-12 10:18:37 +0900

    第2章の練習問題を提出

---
+++ ControlCheck/ControlCheck/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //CheckBox1_CheckedChangedイベントハンドラ
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            labelCheckBox.Text = "チェックボックス：" + checkBox1.Checked;
+        }
+
+        //radioButton1_CheckedChangedイベントハンドラ
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            labelRadioButton1.Text = "チェックボックス：" + radioButton1.Checked;
+        }
+
+        //radioButton2_CheckedChangedイベントハンドラ
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            labelRadioButton2.Text = "チェックボックス：" + radioButton2.Checked;
+        }
+
+        //numericUpDown1_CheckedChangedイベントハンドラ
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            labelNumericUpDown.Text = "チェックボックス：" + numericUpDown1.Value;
+        }
+    }
+}

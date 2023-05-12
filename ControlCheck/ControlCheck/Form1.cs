b0ffc7c851617d7bba180eca45346458822a3d19
22410034-watanabeyuta <yuta_873@icloud.com>
2023-05-12 10:30:14 +0900

    第二章練習問題提出

---
+++ ControlCheck/ControlCheck/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlCheck
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void labelCheckBox_click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			labelCheckBox.Text = "チェックボックス：" + checkBox1.Checked;
+			labelRadioButton1.Text = "ラジオボタン１：" + radioButton1.Checked;
+			labelRadioButton2.Text = "ラジオボタン２：" + radioButton2.Checked;
+			labelNumericUpDown.Text = "数値：" + numericUpDown1.Value;
+		}
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			labelCheckBox.Text = "チェックボックス：" + checkBox1.Checked;
+		}
+
+		private void radioButton1_CheckedChanged(object sender, EventArgs e)
+		{
+			labelRadioButton1.Text = "ラジオボタン１：" + radioButton1.Checked;
+		}
+
+		private void radioButton2_CheckedChanged(object sender, EventArgs e)
+		{
+			labelRadioButton2.Text = "ラジオボタン２：" + radioButton2.Checked;
+		}
+
+		private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+		{
+			labelNumericUpDown.Text = "数値：" + numericUpDown1.Value;
+		}
+
+		private void LabelRadioButton1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void LabelRadioButton2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void LabelNumericUpDown_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}

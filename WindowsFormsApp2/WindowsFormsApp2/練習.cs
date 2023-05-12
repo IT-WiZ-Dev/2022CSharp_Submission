8b645ded1a2c1c38a8a631a30167a8c1b35a8681
MSI\yogiashi_kota <yogi_kolta@icloud.com>
2023-05-12 10:07:11 +0900

    第二章練習問題提出

---
+++ WindowsFormsApp2/WindowsFormsApp2/練習.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelCheckBox.Text = "チェックボックス：" + checkBox1.Checked;
+
+            labelRadioButton1.Text = "ラジオボタン１：" + radioButton1.Checked;
+
+            labelRadioButton2.Text = "ラジオボタン２：" + radioButton2.Checked;
+
+            labelNumericUpDown.Text = "数値：" + numericUpDown1.Value;
+        }
+
+        private void labelCheckBox_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void labelRadioButton1_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void labelRadioButton2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void labelNumericUpDown_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}

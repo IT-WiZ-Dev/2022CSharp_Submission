c2e65dc769078c9bee5ed80c5804a9b4ce9edc9c
MSI\ryohe <zeal22310169@icloud.com>
2023-05-18 15:07:44 +0900

    第三章練習問題追加

---
+++ SplitCost/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int money;
+            double addTax;
+            const double Tax = 0.1;
+
+            money = int.Parse(textBox1.Text);
+
+            addTax = money;
+            addTax *= (1 + Tax);
+            money = (int)addTax;
+
+            int okane;
+
+            okane = int.Parse(textBox2.Text);
+
+            int hitori;
+
+            hitori = money / okane;
+
+            int hutari;
+
+            hutari = money % okane;
+
+            label7.Text = hitori + "円";
+            label8.Text = hutari + "円";
+        }
+    }
+}

65879495cc6ed71071753c593d440468ff1c1931
MSI\明坂　翔 <zhongx457@gmail.com>
2023-05-18 14:36:39 +0900

    練習問題３提出しました。

---
+++ SplitCost/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplitCost
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int money;
+
+            int hito;
+
+            int ans1;
+
+            int ans2;
+
+            double add;
+
+            const double Tax = 1.1;
+
+
+            money = int.Parse(textBox1.Text);
+         
+            add = money;
+
+            add = add * Tax;
+
+            money = (int)add;
+
+            hito = int.Parse(textBox2.Text);
+
+            ans1 = money / hito;
+
+            ans2 = money % hito;
+
+
+            label7.Text = ans1 + "円";
+
+            label8.Text = ans2 + "円";
+
+
+        }
+    }
+}

308af0efa637423e93ebc5cd3ba1f21d125204e2
DESKTOP-87LMO7N\akary <Rkawaai0105@gmail.com>
2023-06-02 10:02:24 +0900

    第4章練習問題を提出

---
+++ WeekOfDay/WeekOfDay/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace WeekOfDay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.text = new System.Windows.Forms.TextBox();
+            this.NumericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.NumericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.ans = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "西暦年";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "月";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(269, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "日";
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(28, 64);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(97, 25);
+            this.text.TabIndex = 1;
+            // 
+            // NumericUpDown1
+            // 
+            this.NumericUpDown1.Location = new System.Drawing.Point(167, 64);
+            this.NumericUpDown1.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.NumericUpDown1.Name = "NumericUpDown1";
+            this.NumericUpDown1.Size = new System.Drawing.Size(63, 25);
+            this.NumericUpDown1.TabIndex = 2;
+            // 
+            // NumericUpDown2
+            // 
+            this.NumericUpDown2.Location = new System.Drawing.Point(272, 64);
+            this.NumericUpDown2.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.NumericUpDown2.Name = "NumericUpDown2";
+            this.NumericUpDown2.Size = new System.Drawing.Size(63, 25);
+            this.NumericUpDown2.TabIndex = 2;
+            // 
+            // ans
+            // 
+            this.ans.AutoSize = true;
+            this.ans.Location = new System.Drawing.Point(379, 64);
+            this.ans.Name = "ans";
+            this.ans.Size = new System.Drawing.Size(0, 18);
+            this.ans.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(461, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "曜日算出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 175);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ans);
+            this.Controls.Add(this.NumericUpDown2);
+            this.Controls.Add(this.NumericUpDown1);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.NumericUpDown NumericUpDown1;
+        private System.Windows.Forms.NumericUpDown NumericUpDown2;
+        private System.Windows.Forms.Label ans;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WeekOfDay/WeekOfDay/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeekOfDay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ans.Text = "";
+
+
+        }
+
+
+        //メインボタンを押したとき
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //年月日を整列型に変換
+            int year;
+            int month = (int)NumericUpDown1.Value;
+            int day = (int)NumericUpDown2.Value;
+
+            //西暦年エラーチェック
+            if (int.TryParse(text.Text, out year))
+            {
+                //int型に変換済み
+            }
+            else
+            {
+                ans.Text = "西暦年エラー";
+                return;
+            }
+
+            if(year < 0)
+            {
+                ans.Text = "西暦年エラー";
+                return;
+            }
+
+            //日付チェック
+            if(ValCheck(year, month, day) == false)
+            {
+                ans.Text = "あり得ない日付";
+                return;
+            }
+
+
+            //結果
+            ans.Text = DayOfWeek(year, month, day);
+
+        }
+
+
+
+        //閏年かを判定するメソッド
+        private Boolean UruCheck (int year)
+        {
+            int a = year / 4;
+            int b = year / 100;
+            int c = year / 400;
+
+            if(a == 0 && b != 0)
+            {
+                return true;
+            }
+
+            else if(c == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        //年月日の妥当性チェック
+        private Boolean ValCheck (int year, int month, int day)
+        {
+            switch(month)
+            {
+                case 1:
+                    if(day >= 1 && day <= 31)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case 2:
+                    //閏年の場合
+                    if(UruCheck(year) == true)
+                    {
+                        if(day >= 1 && day <= 29)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    //閏年でない場合
+                    else if(UruCheck(year) == false)
+                    {
+                        if (day >= 1 && day <= 28)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case 3:
+                    if (day >= 1 && day <= 31)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case 4:
+                    if (day >= 1 && day <= 30)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case 5:
+                    if (day >= 1 && day <= 31)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case 6:
+                    if (day >= 1 && day <= 30)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case 7:
+                    if (day >= 1 && day <= 31)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case 8:
+                    if (day >= 1 && day <= 31)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case 9:
+                    if (day >= 1 && day <= 30)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case 10:
+                    if (day >= 1 && day <= 31)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case 11:
+                    if (day >= 1 && day <= 30)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case 12:
+                    if (day >= 1 && day <= 31)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                default:
+                    return false;
+            }
+        }
+
+
+        //曜日を見つけるメソッド
+        private string DayOfWeek(int year, int month, int day)
+        {
+
+            int w = (5 * year / 4 - year / 100 + year / 400 + (26 * month + 16) / 10 + day) % 7;
+
+            if(month == 1)
+            {
+                year = year - 1;
+                w = (5 * year / 4 - year / 100 + year / 400 + (26 * 13 + 16) / 10 + day) % 7;
+            }
+            else if (month == 2)
+            {
+                year = year - 1;
+                w = (5 * year / 4 - year / 100 + year / 400 + (26 * 14 + 16) / 10 + day) % 7;
+            }
+
+
+            switch (w)
+            {
+                case 0:
+                    return "日曜日です";
+
+                case 1:
+                    return "月曜日です";
+
+                case 2:
+                    return "火曜日です";
+
+                case 3:
+                    return "水曜日です";
+
+                case 4:
+                    return "木曜日です";
+
+                case 5:
+                    return "金曜日です";
+
+                case 6:
+                    return "土曜日です";
+
+                default:
+                    return "";
+            }
+        }
+    }
+}

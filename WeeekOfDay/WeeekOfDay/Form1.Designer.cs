6717f769b51cd0f8d47b35f375ee1520400c8469
晃太朗 <zeal22410203@fsgcl.jp>
2023-06-02 09:50:49 +0900

    第4章練習問題を追加

---
+++ WeeekOfDay/WeeekOfDay/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace WeeekOfDay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelYear = new System.Windows.Forms.Label();
+            this.labelMonth = new System.Windows.Forms.Label();
+            this.labelDay = new System.Windows.Forms.Label();
+            this.labelYoubi = new System.Windows.Forms.Label();
+            this.textBoxYear = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDownMonth = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownDay = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelYear
+            // 
+            this.labelYear.AutoSize = true;
+            this.labelYear.Location = new System.Drawing.Point(44, 31);
+            this.labelYear.Name = "labelYear";
+            this.labelYear.Size = new System.Drawing.Size(62, 18);
+            this.labelYear.TabIndex = 0;
+            this.labelYear.Text = "西暦年";
+            // 
+            // labelMonth
+            // 
+            this.labelMonth.AutoSize = true;
+            this.labelMonth.Location = new System.Drawing.Point(185, 31);
+            this.labelMonth.Name = "labelMonth";
+            this.labelMonth.Size = new System.Drawing.Size(26, 18);
+            this.labelMonth.TabIndex = 1;
+            this.labelMonth.Text = "月";
+            // 
+            // labelDay
+            // 
+            this.labelDay.AutoSize = true;
+            this.labelDay.Location = new System.Drawing.Point(287, 31);
+            this.labelDay.Name = "labelDay";
+            this.labelDay.Size = new System.Drawing.Size(26, 18);
+            this.labelDay.TabIndex = 2;
+            this.labelDay.Text = "日";
+            // 
+            // labelYoubi
+            // 
+            this.labelYoubi.AutoSize = true;
+            this.labelYoubi.Location = new System.Drawing.Point(377, 67);
+            this.labelYoubi.Name = "labelYoubi";
+            this.labelYoubi.Size = new System.Drawing.Size(43, 18);
+            this.labelYoubi.TabIndex = 3;
+            this.labelYoubi.Text = "label";
+            this.labelYoubi.Click += new System.EventHandler(this.labelYoubi_Click);
+            // 
+            // textBoxYear
+            // 
+            this.textBoxYear.Location = new System.Drawing.Point(44, 64);
+            this.textBoxYear.Name = "textBoxYear";
+            this.textBoxYear.Size = new System.Drawing.Size(94, 25);
+            this.textBoxYear.TabIndex = 4;
+            this.textBoxYear.Text = "2022";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(541, 41);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "曜日算出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDownMonth
+            // 
+            this.numericUpDownMonth.Location = new System.Drawing.Point(188, 64);
+            this.numericUpDownMonth.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDownMonth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownMonth.Name = "numericUpDownMonth";
+            this.numericUpDownMonth.Size = new System.Drawing.Size(65, 25);
+            this.numericUpDownMonth.TabIndex = 10;
+            this.numericUpDownMonth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownDay
+            // 
+            this.numericUpDownDay.Location = new System.Drawing.Point(290, 64);
+            this.numericUpDownDay.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.numericUpDownDay.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownDay.Name = "numericUpDownDay";
+            this.numericUpDownDay.Size = new System.Drawing.Size(58, 25);
+            this.numericUpDownDay.TabIndex = 11;
+            this.numericUpDownDay.TabStop = false;
+            this.numericUpDownDay.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 163);
+            this.Controls.Add(this.numericUpDownDay);
+            this.Controls.Add(this.numericUpDownMonth);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxYear);
+            this.Controls.Add(this.labelYoubi);
+            this.Controls.Add(this.labelDay);
+            this.Controls.Add(this.labelMonth);
+            this.Controls.Add(this.labelYear);
+            this.Name = "Form1";
+            this.Text = "曜日算出";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelYear;
+        private System.Windows.Forms.Label labelMonth;
+        private System.Windows.Forms.Label labelDay;
+        private System.Windows.Forms.Label labelYoubi;
+        private System.Windows.Forms.TextBox textBoxYear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDownMonth;
+        private System.Windows.Forms.NumericUpDown numericUpDownDay;
+    }
+}
+
---
+++ WeeekOfDay/WeeekOfDay/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeeekOfDay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void listBoxMonth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelYoubi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelYoubi.Text = "";
+        }
+
+        private int uruJuge(string text)
+        {
+            if (!int.TryParse(text, out int years))
+            {
+                return -1; // エラーを示す値を返す
+            }
+
+            int a = years % 4;
+            int b = years % 100;
+            int c = years % 400;
+            int x = 0;
+            if (a == 0 && b != 0 || c == 0)
+            {
+                x = 1;
+            }
+            return x;
+        }
+
+
+        private string monthdaysJuge(int month, int days, int y)
+        {
+            int d = 0;
+            string g = "";
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            {
+                d = 31;
+            }
+            else if (month == 4 || month == 6 || month == 9 || month == 11)
+            {
+                d = 30;
+            }
+            else if (month == 2 && y == 1)
+            {
+                d = 29;
+            }
+            else if (month == 2 && y == 0)
+            {
+                d = 28;
+            }
+            if (days > d)
+            {
+                g = "あり得ない日付";
+            }
+            return g;
+        }
+
+        private string youbiJuge(int years, int month, int days)
+        {
+            int w = 0;
+            if (month == 1 || month == 2)
+            {
+                years = years - 1;
+                month = month + 12;
+            }
+            w = (5 * years
+                / 4 - years / 100 + years / 400 + (26 * month + 16) / 10 + days) % 7;
+
+            string[] daysOfWeek = { "日曜日です", "月曜日です", "火曜日です", "水曜日です", "木曜日です", "金曜日です", "土曜日です" };
+            string k = daysOfWeek[w];
+            return k;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //うるう年判定
+            int y;
+            y = uruJuge(textBoxYear.Text);
+
+            if (!int.TryParse(textBoxYear.Text, out int years) || years <= 0)
+            {
+                labelYoubi.Text = "西暦年エラー";
+                return;
+            }
+
+            int month = (int)numericUpDownMonth.Value;
+            int days = (int)numericUpDownDay.Value;
+            string f;
+            f = monthdaysJuge(month, days, y);
+            labelYoubi.Text += f;
+
+            if (labelYoubi.Text == "")
+            {
+                string k = "";
+                k = youbiJuge(years, month, days);
+                labelYoubi.Text = k;
+            }
+
+
+        }
+    }
+}

b15d539c0ca3e6290ae8fe64f58a88c42a03e78b
orikasa22310184 <koki.orikasa0612@gmail.com>
2023-06-02 12:20:31 +0900

    第5章練習問題の提出

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        const int answer = 76; // 当たりの数
+        int guess;
+        int attempts = 0;
+
+        Console.WriteLine("0から100の範囲の数を当ててください。");
+
+        do
+        {
+            Console.Write("> ");
+            string input = Console.ReadLine();
+
+            // 入力が数値かどうかを確認
+            if (!int.TryParse(input, out guess))
+            {
+                Console.WriteLine("数値を入力してください。");
+                continue;
+            }
+
+            // 範囲外の数を入力した場合は再入力を求める
+            if (guess < 0 || guess > 100)
+            {
+                Console.WriteLine("0から100の範囲で入力してください。");
+                continue;
+            }
+
+            attempts++;
+
+            if (guess > answer)
+            {
+                Console.WriteLine("答えはもっと小さいです。");
+            }
+            else if (guess < answer)
+            {
+                Console.WriteLine("答えはもっと大きいです。");
+            }
+            else
+            {
+                Console.WriteLine("正解です！ {0}回目で当たりました。", attempts);
+            }
+
+        } while (guess != answer);
+
+        Console.ReadLine();
+    }
+}
---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 九九の表を表示
+        Console.WriteLine("***九九の表***");
+
+        // 横に1～9の数字を表示
+        Console.Write("   |");
+        for (int i = 1; i <= 9; i++)
+        {
+            Console.Write("{0,3}", i);
+        }
+        Console.WriteLine();
+
+        // 横線を表示
+        Console.WriteLine("--------------------------------");
+
+        // 縦と計算結果を表示
+        for (int i = 1; i <= 9; i++)
+        {
+            Console.Write("{0,2} |", i);
+            for (int j = 1; j <= 9; j++)
+            {
+                Console.Write("{0,3}", i * j);
+            }
+            Console.WriteLine(); // 改行
+        }
+
+        // キー入力待ち
+        Console.WriteLine("終了するには何かキーを押してください。");
+        Console.ReadKey();
+    }
+}

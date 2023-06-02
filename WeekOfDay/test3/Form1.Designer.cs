c0f1c7a4349519b2f7a2dc63f3cc9b0fce542381
二階堂祐太 <yuuta.nika.0701@gmail.com>
2023-06-02 09:50:29 +0900

    第四章練習問題

---
+++ WeekOfDay/test3/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+namespace WeekdayCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.numMonth = new System.Windows.Forms.NumericUpDown();
+            this.numDay = new System.Windows.Forms.NumericUpDown();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numMonth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(12, 17);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(100, 25);
+            this.txtYear.TabIndex = 0;
+            // 
+            // numMonth
+            // 
+            this.numMonth.Location = new System.Drawing.Point(133, 19);
+            this.numMonth.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numMonth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numMonth.Name = "numMonth";
+            this.numMonth.Size = new System.Drawing.Size(50, 25);
+            this.numMonth.TabIndex = 1;
+            this.numMonth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numDay
+            // 
+            this.numDay.Location = new System.Drawing.Point(213, 17);
+            this.numDay.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.numDay.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numDay.Name = "numDay";
+            this.numDay.Size = new System.Drawing.Size(50, 25);
+            this.numDay.TabIndex = 2;
+            this.numDay.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(287, 14);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(98, 32);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "曜日算出";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 45);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 18);
+            this.lblResult.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(436, 137);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.numDay);
+            this.Controls.Add(this.numMonth);
+            this.Controls.Add(this.txtYear);
+            this.Name = "Form1";
+            this.Text = "Weekday Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.numMonth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ WeekOfDay/test3/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WeekdayCalculator
+{
+    public partial class Form1 : Form
+    {
+        private TextBox txtYear;
+        private NumericUpDown numMonth;
+        private NumericUpDown numDay;
+        private Button btnCalculate;
+        private Label lblResult;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int year, month, day;
+            if (!int.TryParse(txtYear.Text, out year) || year < 0)
+            {
+                lblResult.Text = "西暦年エラー";
+                return;
+            }
+
+            month = (int)numMonth.Value;
+            day = (int)numDay.Value;
+
+            if (!IsValidDate(year, month, day))
+            {
+                lblResult.Text = "あり得ない日付";
+                return;
+            }
+
+            string weekday = GetWeekday(year, month, day);
+            lblResult.Text = weekday;
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        }
+
+        private bool IsValidDate(int year, int month, int day)
+        {
+            if (month == 2)
+            {
+                if (IsLeapYear(year))
+                    return day <= 29;
+                else
+                    return day <= 28;
+            }
+            else if (month == 4 || month == 6 || month == 9 || month == 11)
+            {
+                return day <= 30;
+            }
+            else
+            {
+                return day <= 31;
+            }
+        }
+
+        private string GetWeekday(int year, int month, int day)
+        {
+            int[] monthTable = { 0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5 };
+            int centuryCode = 6 - 2 * ((year / 100) % 4);
+            int yearCode = (year % 100 + (year % 100) / 4) % 7;
+            int monthCode = monthTable[month - 1];
+            int leapYearCorrection = (IsLeapYear(year) && (month == 1 || month == 2)) ? -1 : 0;
+
+            int weekdayCode = (centuryCode + yearCode + monthCode + day + leapYearCorrection) % 7;
+
+            string[] weekdays = { "土曜日です", "日曜日です", "月曜日です", "火曜日です", "水曜日です", "木曜日です", "金曜日です" };
+
+            return weekdays[weekdayCode];
+        }
+    }
+}

a759e219e08c2cf1ccc2decd985946ea05dbad61
MSI\81905 <zeal22410204@fsgcl.jp>
2023-06-02 12:23:51 +0900

    5章練習問題提出

---
+++ Guess/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int A = r.Next(0,100);
+            int B = 101;
+            int Count = 0;
+            while (A != B)
+            {
+                B = int.Parse(Console.ReadLine());
+                if (B >100 || B < 0)
+                {
+                    Console.WriteLine("入力値が正しくないです");
+                }
+                if (A < B)
+                {
+                    Console.WriteLine("答えより大きいです");
+                }
+                else if(A > B)
+                {
+                    Console.WriteLine("答えより小さいです");
+                }
+                Count++;
+            }
+            Console.WriteLine(Count + "回目で当たりました");
+            Console.Write('\n');
+        }
+    }
+}
---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("　　   ***九九の表***　　");
+            Console.WriteLine("   |  1  2  3  4  5  6  7  8  9");
+            Console.WriteLine("--------------------------------");
+            for (int i = 1;i <= 9; i++)
+            {
+                Console.Write(i + "  |");
+                for (int j = 1;j <= 9;j++)
+                {
+                    if (j == 9)
+                    {
+                        if (i*j >= 10)
+                        {
+                            Console.WriteLine(" " + i * j);
+                        }
+                        else
+                        {
+                            Console.WriteLine("  " + i * j);
+                        }
+                    }
+                    else
+                    {
+                        if (i*j >= 10)
+                        {
+                            Console.Write(" " + i * j);
+                        }
+                        else
+                        {
+                            Console.Write("  " + i * j);
+                        }
+                    }
+                }
+            }
+                    
+        }
+    }
+}

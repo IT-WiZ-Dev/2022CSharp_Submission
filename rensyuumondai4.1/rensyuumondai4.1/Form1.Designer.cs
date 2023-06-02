eca0deee02b9765512db5900736a6ffff0003e76
sakuma0801 <take.03081.s@gmail.com>
2023-06-02 09:46:30 +0900

    第４章練習問題を提出

---
+++ rensyuumondai4.1/rensyuumondai4.1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace rensyuumondai4._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "西暦年";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "計算";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(323, 112);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(85, 22);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(320, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "月";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(552, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "日";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(737, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "です";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(555, 111);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(70, 22);
+            this.numericUpDown2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ rensyuumondai4.1/rensyuumondai4.1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rensyuumondai4._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int Form1_Load(object sender, EventArgs e)
+        {
+
+
+            int y, m, d, days, n;
+            char buf[BUFFER_SIZE], c;
+
+            printf("Please input days\n");
+            while (1)
+            {
+                fgets(buf, BUFFER_SIZE, stdin);
+                n = sscanf(buf, "%d %c", &days, &c);
+                if (n == 1)
+                {
+                    break;
+                }
+                else if (n == -1)
+                {
+                    continue;
+                }
+                else
+                {
+                    printf("Usage:days\n");
+                    return 0;
+                }
+            }
+
+            if (days < 0)
+            {
+                printf("error:days\n");
+                return 0;
+            }
+            y = (days + 305) / 146097 * 400
+                + (days + 305) % 146097 / 36524 * 100
+                + (days + 305) % 146097 % 36524 / 1461 * 4
+                + (days + 305) % 146097 % 36524 % 1461 / 365;
+            d = days - (365 * (y - 1) + y / 4 - y / 100 + y / 400 + 31 + 28 + 1);
+            if (d == -1)
+            {
+                y--;
+                d = 365;
+            }
+            for (m = 3; m < 15; m++)
+            {
+                if (306 * (m + 1) / 10 - 122 <= d && 306 * (m + 2) / 10 - 122 > d)
+                    break;
+            }
+            d -= 306 * (m + 1) / 10 - 122 - 1;
+            if (m > 12)
+            {
+                y++;
+                m -= 12;
+            }
+            printf("%d年%d月%d日\n", y, m, d);
+            switch (days % 7)
+            {
+                case 0:
+                    label4.Text="日曜日です";
+                    break;
+                case 1:
+                    label4.Text = "火曜日です";
+                    break;
+                case 2:
+                    label4.Text = "水曜日です";
+                    break;
+                case 3:
+                    label4.Text = "木曜日です";
+                    break;
+                case 4:
+                    label4.Text = "金曜日です";
+                    break;
+                case 5:
+                    label4.Text = "土曜日です";
+                    break;
+                case 6:
+                    label4.Text = "日曜日です";
+                    break;
+            }
+            return 0;
+        }
+
+
+
+    }
+}
+}

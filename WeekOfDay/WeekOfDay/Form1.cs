f445c4a515af22111e240bf309c9d7602df82ff7
kaji yousuke <kaji09@outlook.jp>
2023-06-02 09:50:42 +0900

    第4章練習問題の追加

---
+++ WeekOfDay/WeekOfDay/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeekOfDay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Text = "";
+        }
+
+        private void TextToValue(string text,out int val)
+        {
+            if (int.TryParse(text, out val) == false)
+                val = -1;
+        }
+    }
+}

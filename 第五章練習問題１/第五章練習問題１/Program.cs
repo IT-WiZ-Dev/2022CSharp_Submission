b8ec5dc64179431709f342e189f261e64a5a2720
MSI\namik <zeal22410092@fsgcl.jp>
2023-06-02 11:06:34 +0900

    第五章練習問題1を追加

---
+++ 第五章練習問題１/第五章練習問題１/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第五章練習問題１
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** 九九の表 ***");
+
+            // 横の数字を表示
+            Console.Write("  ");
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write($"{i}  ");
+            }
+            Console.WriteLine();
+
+            // 表の区切り線を表示
+            Console.WriteLine("---------------------------");
+
+            // 九九の表を表示
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write($"{i}|");
+                for (int j = 1; j <= 9; j++)
+                {
+                    int result = i * j;
+                    Console.Write($"{result,2} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}

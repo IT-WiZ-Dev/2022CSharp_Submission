ac7d49e1f4993bb04b09242d5c1aa09196dfcd4a
kaji yousuke <kaji09@outlook.jp>
2023-06-08 14:32:49 +0900

    第５章の練習問題１の提出

---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("＊＊＊九九の表＊＊＊");
+            Console.WriteLine();
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.Write($"{i * j}\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("終了するには何かキーを押してください。");
+            Console.ReadKey();
+        }
+    }
+}

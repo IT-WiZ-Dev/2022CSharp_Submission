b37b6d3d57524671e659b50102180232a1a59532
DESKTOP-485HM6J\Owner <ryota.hashi43@gmail.com>
2023-06-08 14:38:14 +0900

    第五章練習問題を追加

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 32;
+            int a = 0;
+            int b = 100;
+            Console.WriteLine(a + "から" + b +"の間の数値を当ててください。>");
+            int score = int.Parse(Console.ReadLine());
+            int count = 1;
+            while(n != score)
+            {
+                if(score > b || a < 0 || score < a)
+                {
+                    Console.WriteLine("入力値が正しくありません。再入力してください。");
+                }
+                else
+                {
+                    if(n < score)
+                    {
+                        Console.WriteLine("答はもっと小さいです");
+                        b = score - 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("答はもっと大きいです");
+                        a = score + 1;
+                    }
+                }
+                Console.WriteLine(a + "から" + b + "の間の数値を当ててください。>");
+                score = int.Parse(Console.ReadLine());
+                count += 1;
+            }
+            Console.WriteLine("おめでとう。"+ count + "回目で当たりました。");
+            Console.Write('\n');
+            Console.WriteLine("終了するには何かキーを押してください。");
+            Console.Read();
+        }
+    }
+}
---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   * * * 九九の表 * * *   ");
+            Console.WriteLine("   |  1  2  3  4  5  6  7  8  9");
+            Console.WriteLine("--------------------------------");
+            for (int a = 1; a <= 9; a ++)
+            {
+                Console.Write(a);
+                Console.Write("  |");
+                for (int b = 1; b <= 9; b++)
+                {
+                    int n = 0;
+                    n = a * b;
+                    Console.Write("{0,3}", n);
+                }
+                Console.Write('\n');
+            }
+            Console.WriteLine("終了するには何かキーを押してください。");
+            Console.Read();
+        }
+    }
+}

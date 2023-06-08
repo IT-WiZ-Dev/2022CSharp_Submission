824f7ac3f80311b1a1cfc54da6f7f264cf8fc540
MSI\deoki <deokishisu1220@gmail.com>
2023-06-08 15:26:27 +0900

    第5章練習問題提出

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int ans = 30;
+            int min = 0;
+            int max = 100;
+            int x;
+            int n = 1;
+            while(true)
+            {
+                Console.Write(min + "から" + max + "の間の数値を当ててください。＞ ");
+                x = int.Parse(Console.ReadLine());
+                if (x < min || max < x)
+                {
+                    Console.Write("入力値が正しくありません。再入力してください。");
+                }
+                else if (ans == x)
+                {
+                    Console.Write("おめでとう。" + n + "回目で当たりました。");
+                    break;
+                }
+                else if (ans < x)
+                {
+                    Console.WriteLine("答はもっと小さいです");
+                    max = x - 1;
+                    n++;
+                }
+                else
+                {
+                    Console.WriteLine("答はもっと大きいです");
+                    min = x + 1;
+                    n++;
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+            Console.Write("終了するには何かキーを押してください。");
+            Console.Read();
+        }
+    }
+}
---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("   ");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("* ");
+            }
+            Console.Write("九九の表");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(" *");
+            }
+            Console.Write("\n");
+            Console.Write("   |");
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.Write("{0,3}",i);
+            }
+            Console.Write("\n");
+            for (int i = 0; i < 32; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("\n");
+            for (int i = 1; i < 10; i++)
+            {
+                Console.Write(i+ "  |");
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.Write("{0,3}", i*j);
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+            Console.Write("終了するには何かキーを押してください。");
+            Console.Read();
+        }
+    }
+}

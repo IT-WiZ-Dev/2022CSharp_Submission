6d99fb56eda82e61a8587611204a75b9c033ce84
Wiz22Konno <emailnoreply85@gmail.com>
2023-06-08 15:59:43 +0900

    教科書第5章練習問題2つを提出

---
+++ 教科書第5章練習問題(Guess)/教科書第5章練習問題(Guess)/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 教科書第5章練習問題_Guess_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Atari = 39;
+            int Max = 100;
+            int Min = 0;
+            int Count = 1;
+            int n1;
+            int Judge = 0;
+
+            do
+            {
+
+                Console.Write(Min + "から" + Max + "の間の数値を当ててください。＞");
+                n1 = int.Parse(Console.ReadLine());
+
+                if (n1 < Min || n1 > Max)
+                {
+                    Console.WriteLine("入力値が正しくありません。再入力してください。");
+                    Count = Count + 1;
+                    continue;
+                }
+
+                else if (n1 > Atari)
+                {
+                    Console.WriteLine("答はもっと小さいです。");
+                    Max = n1 - 1;
+                    Count = Count + 1;
+                    continue;
+                }
+                else if (n1 < Atari)
+                {
+                    Console.WriteLine("答はもっと大きいです。");
+                    Min = n1 + 1;
+                    Count = Count + 1;
+                    continue;
+                }
+                else if (n1 == Atari)
+                {
+                    Console.WriteLine("おめでとう。" + Count + "回目で当たりました。");
+                    Judge = Judge + 1;
+                }
+            } while (Judge != 1);
+            Console.WriteLine("終了するには何かキーを押してください");
+            Console.Read();
+        }
+    }
+  }
+    
---
+++ 教科書第5章練習問題(MultiplicationTable)/教科書第5章練習問題(MultiplicationTable)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 教科書第5章練習問題_MultiplicationTable_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   * * * 九九の表 * * *");
+            Console.WriteLine("   |  1  2  3  4  5  6  7  8  9");
+            Console.WriteLine("--------------------------------");
+
+            int val;
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write(i + "  | ");
+
+                for (int j = 1; j <= 9; j++)
+                {
+                    val = i * j;
+                    System.Console.Write("{0,3}", val.ToString() + " ");
+                }
+                System.Console.Write("\n");
+            }
+
+            Console.WriteLine("終了するには何かキーを押してください。");
+            Console.Read();
+        }
+    }
+}

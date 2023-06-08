0da2a2f0ba5ab70e31bc15680a09a8bc4129cb56
MSI\yogiashi_kota <yogi_kolta@icloud.com>
2023-06-08 15:39:03 +0900

    第五章　練習問題追加

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ***九九の表*** ");
+
+            Console.Write("\t");
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write("{0}\t", i);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("----------------------------------------------------------------------------");
+
+            // 九九の表を作成するためのループ
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write("{0} |\t", i);
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.Write("{0,3}\t", i * j);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("終了するには何かキーを押してください。");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 数当てゲーム/数当てゲーム/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 数当てゲーム
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = 58;
+            int min = 0;
+            int max = 100;
+            int n1;
+            int cnt = 0;
+            try
+            {
+                do
+                {
+                    Console.Write(min + "から" + max + "の間の数値を当ててください＞");
+                    n1 = int.Parse(Console.ReadLine());
+
+                    if (n1 >= min && n1 <= max)
+                    {
+                        if (n1 > key)
+                        {
+                            max = n1;
+                            Console.Write("答えはもっと小さいです");
+                            cnt = cnt + 1;
+                            Console.ReadLine();
+                        }
+                        else if(n1 < key)
+                        {
+                            min = n1;
+                            Console.Write("答えはもっと大きいです");
+                            cnt = cnt + 1;
+                            Console.ReadLine();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("入力値が正しくありません、再入力してください＞");
+                        continue;
+                    }
+
+                } while (n1 != key);
+                cnt = cnt + 1;
+                Console.Write("おめでとう。" + cnt + "回目で当たりました。");
+                Console.Write("終了するには何かキーを押してください");
+                Console.ReadLine();
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+               
+            }
+        }
+    }
+}

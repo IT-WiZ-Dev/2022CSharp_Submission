efd06765e02241f345d7aac782e77a2b4992413f
MSI\droid <droid0614@outlook.jp>
2023-06-08 16:05:23 +0900

    練習問題5を追加

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class Program
+{
+    Random random;
+    readonly int answer;
+    int minRange = 0; // 範囲の最小値
+    int maxRange = 100; // 範囲の最大値
+
+    public Program()
+    {
+        random = new Random();
+        answer = random.Next(1, 101);
+    }
+
+    static void Main()
+    {
+        Program program = new Program();
+        program.StartGame();
+
+        Console.ReadLine();
+    }
+
+    void StartGame()
+    {
+        int guess;
+        int attempts = 0;
+
+       
+
+        do
+        {
+            Console.Write("{0}から{1}の間で数値を当ててください。", minRange, maxRange);
+            Console.Write("＞");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out guess))
+            {
+                Console.WriteLine("入力値が正しくありません。再入力してください。");
+                continue;
+            }
+
+            if (guess < minRange || guess > maxRange)
+            {
+                Console.WriteLine("{0}から{1}の間で数値を入力してください。", minRange, maxRange);
+                continue;
+            }
+
+            attempts++;
+
+            if (guess < answer)
+            {
+                Console.WriteLine("答はもっと大きいです。");
+                minRange = guess + 1;
+            }
+            else if (guess > answer)
+            {
+                Console.WriteLine("答はもっと小さいです。");
+                maxRange = guess - 1;
+            }
+            else
+            {
+                Console.WriteLine("おめでとう。{0}回目で当たりました。", attempts);
+            }
+
+        } while (guess != answer);
+    }
+}
---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t＊＊＊九九の表＊＊＊");
+
+    
+        for (int i = 1; i <= 9; i++)
+        {
+            Console.Write($"　{i,2}");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("　|----------------------------------"); // 線
+
+        for (int i = 1; i <= 9; i++)
+        {
+            Console.Write($"{i,2}|"); // 左横の見出しの位置
+            for (int j = 1; j <= 9; j++)
+            {
+                Console.Write($"{i * j,2}　");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}

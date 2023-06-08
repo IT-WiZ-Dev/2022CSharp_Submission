c3e0ad3ea6a38076e9a43fabbdad9c46a884ffcf
kikuti <kikuti07012003@outlook.jp>
2023-06-08 16:04:01 +0900

    ５演習問題

---
+++ Multiplication Table/Multiplication Table/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 9; // 九九のサイズ
+
+            Console.WriteLine("*** 九九の表 ***");
+          
+            
+            Console.Write("  ");
+           
+            for (int i = 1; i <= size; i++)
+            {
+                
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------");
+
+
+            for (int i = 1; i <= size; i++)
+            {
+                Console.Write($"{i,2}|");
+                for (int j = 1; j <= size; j++)
+                {
+                    int result = i * j;
+                    Console.Write($"{result,3}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Quess/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int targetNumber = random.Next(1, 101);
+
+            Console.Write("0から100までの数を当ててください。");
+
+            int guessCount = 0;
+            int guessedNumber = 0;
+
+            while (guessedNumber != targetNumber)
+            {
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out guessedNumber))
+                {
+                    Console.Write("入力が無効です。数値を入力してください。");
+                    continue;
+                }
+
+                guessCount++;
+
+                if (guessedNumber < targetNumber)
+                {
+                    Console.WriteLine("もっと大きい数です。");
+                }
+                else if (guessedNumber > targetNumber)
+                {
+                    Console.WriteLine("もっと小さい数です。");
+                }
+                else
+                {
+                    Console.WriteLine($"おめでとうございます！{guessCount}回目で当たりました。");
+                }
+            }
+
+            Console.WriteLine("ゲームを終了します。");
+        }
+    }
+}

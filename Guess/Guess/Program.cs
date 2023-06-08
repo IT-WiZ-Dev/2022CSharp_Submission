5c95bb879c167719170d09d7a66af5c6a2bafe53
MSI\joryu <joryu0725@au.com>
2023-06-08 16:03:57 +0900

    第５章練習問題提出

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = 0;
+            int maxNumber = 100;
+            int targetNumber;
+            int attempts = 0;
+
+            Random random = new Random();
+            targetNumber = random.Next(minNumber, maxNumber + 1);
+
+            Console.WriteLine("数当てゲームを開始します。");
+            Console.WriteLine($"{minNumber}から{maxNumber}の間の数値を当ててください。");
+            
+            while (true)
+            {
+                Console.Write("予想した数を入力してください: ");
+                string input = Console.ReadLine();
+                int guess;
+
+                if (!int.TryParse(input, out guess) || guess < minNumber || guess > maxNumber)
+                {
+                    Console.WriteLine("無効な入力です。範囲内の整数を入力してください。");
+                    continue;
+                }
+
+                attempts++;
+
+                if (guess < targetNumber)
+                {
+                    Console.WriteLine("答えはもっと大きいです。");
+                }
+                else if (guess > targetNumber)
+                {
+                    Console.WriteLine("答えはもっと小さいです。");
+                }
+                else
+                {
+                    Console.WriteLine("正解です！おめでとうございます！");
+                    Console.WriteLine($"試行回数: {attempts}");
+                    break;
+                }
+            }
+
+            Console.WriteLine("ゲームを終了します。何かキーを押してください。");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("九九の表");
+
+        // ヘッダーを表示する
+        Console.Write("×\t");
+        for (int i = 1; i <= 9; i++)
+        {
+            Console.Write($"{i}\t");
+        }
+        Console.WriteLine();
+
+        // 区切り線を表示する
+        Console.WriteLine("---------------------------------------------------------------------------");
+
+        // 表の中身を表示する
+        for (int i = 1; i <= 9; i++)
+        {
+            Console.Write($"{i}\t|");
+            for (int j = 1; j <= 9; j++)
+            {
+                Console.Write($"{i * j}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}

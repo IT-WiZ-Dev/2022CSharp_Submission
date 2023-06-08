c65b0b5bb3bdfec59b55856f9d69db16e33966e8
二階堂祐太 <yuuta.nika.0701@gmail.com>
2023-06-08 16:07:50 +0900

    第５章練習問題

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const int answer = 83;
+        int minRange = 0;
+        int maxRange = 100;
+        int attempts = 0;
+
+        Console.WriteLine("０から１００の間の数値を当ててください。");
+
+        while (true)
+        {
+            Console.Write($"＞");
+            string input = Console.ReadLine();
+            if (!int.TryParse(input, out int guess) || guess < minRange || guess > maxRange)
+            {
+                Console.WriteLine("入力値が正しくありません。再入力してください。");
+                continue;
+            }
+
+            attempts++;
+
+            if (guess < answer)
+            {
+                Console.WriteLine($"答はもっと大きいです。\n{guess + 1}から{maxRange}の間の数値を当ててください。");
+                minRange = guess + 1;
+            }
+            else if (guess > answer)
+            {
+                Console.WriteLine($"答はもっと小さいです。\n{minRange}から{guess - 1}の間の数値を当ててください。");
+                maxRange = guess - 1;
+            }
+            else
+            {
+                Console.WriteLine($"おめでとう。{attempts}回目で当たりました。");
+                break;
+            }
+        }
+
+        Console.WriteLine("終了するには何かキーを押してください。");
+        Console.Read();
+    }
+}
---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("     ＊＊＊九九の表＊＊＊");
+        Console.WriteLine("　|      1     2    3    4    5    6    7    8    9");
+        Console.WriteLine("-----------------------------------------");
+
+        for (int i = 1; i <= 9; i++)
+        {
+            Console.Write($"{i}  |");
+            for (int j = 1; j <= 9; j++)
+            {
+                Console.Write($"{i * j,6}");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("終了するには何かキーを押してください。");
+        Console.Read();
+    }
+}

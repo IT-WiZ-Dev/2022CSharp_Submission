8e30fca8992595b639565782540e64ef2ac38d09
MSI\ayaka <usuiayaka22418123@gmail.com>
2023-06-08 14:36:34 +0900

    第5章練習問題を提出

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ans = 75;
+            int count = 1;
+            bool flag = false;
+            while (flag == false)
+            {
+                Console.Write("0から100の間の数値を当ててください。＞");
+                Console.Write('\n');
+                int uesr = int.Parse(Console.ReadLine());
+                if (uesr == ans)
+                {
+                    Console.Write("おめでとう！{0}回目であたりました", count);
+                    Console.Write('\n');
+                    flag = true;
+                }
+                else if (uesr > ans)
+                {
+                    Console.Write("答はもっと小さいです。");
+                    Console.Write('\n');
+                    count++;
+                    continue;
+                }
+                else
+                {
+                    Console.Write("答はもっと大きいです");
+                    Console.Write('\n');
+                    count++;
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("   * * * 九九の表 * * *");
+            Console.Write('\n');
+            Console.Write("   |  1  2  3  4  5  6  7  8  9");
+            Console.Write('\n');
+            Console.Write("_________________________________");
+            Console.Write('\n');
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write("{0}   |",i);
+                for(int j = 1; j <= 9; j++)
+                {
+                    Console.Write("{0,3}",i*j);
+                }
+                Console.Write('\n');
+            }
+            Console.Write('\n');
+
+        }
+    }
+}

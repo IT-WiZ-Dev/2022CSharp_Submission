2244700432ce6c2e5f8a7bdae19ba2d0da12b048
LAPTOP-1LFF53HD\wiz <zeal22310046@fsgcl.jp>
2023-06-08 16:08:14 +0900

    第5章　練習問題

---
+++ Guess2/Guess2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randno = Newnum(1, 101);
+
+            const int num = 3;
+            int count = 0;
+            int low = 0;
+            int high = 100;
+
+
+            while (true)
+            {
+
+                Console.Write("0から100の間の数値を当ててください。 ＞");
+
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                if (input == 0)
+                    Console.WriteLine("入力値が正しくありません。再入力してください。");
+
+                else if (input < randno)
+                {
+                    Console.WriteLine("答はもっと大きいです。");
+                    ++count;
+                    continue;
+                }
+
+                else if (input > randno)
+                {
+                    Console.WriteLine("答はもっと小さいです。");
+                    ++count;
+                    continue;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("おめでとう。" + count + "回目で当たりました。");
+                    break;
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+        }
+
+
+        static int Newnum(int low, int high)
+        {
+           
+            Random random = new Random();
+            
+            return random.Next(low, high);
+
+            
+        }
+
+    }
+}
+
+
---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("＊＊＊九九の表＊＊＊");
+
+            Console.Write(" ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("  {0}", i);
+            }
+            Console.Write("\n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0} ", i);
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i * j < 10)
+                    {
+                        Console.Write(" {0} ", i * j);
+                    }
+                    else
+                    {
+                        Console.Write("{0} ", i * j);
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}

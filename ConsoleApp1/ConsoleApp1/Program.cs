adc921bcf84b84151e46d36438c0a67818f885b7
LAPTOP-1LFF53HD\wiz <zeal22310046@fsgcl.jp>
2023-06-08 14:17:14 +0900

    w

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int price = 
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int sum;
+if(AbbPrice(Price, priceRef, out sum)) 
+{
+    Console.WriteLine( sum );
+}
+
+
+
+
+
+
+
+
+
+.
+
+
+static bool AddPrice (int price1,
+                      int price2,
+                      out int sum)
+                   {
+             sun = price1 + pricw2;
+             return sum >= 0 ? true : false;
+               }
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Text = "";
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Console.Write(TextBox);
+
+            string strYear = Console.ReadLine();
+            int intYear = int.Parse(strYear);
+
+            if ((intYear % 4 == 0) && (intYear % 100 != 0) || (intYear % 400 == 0))
+            {
+                Console.WriteLine();
+
+            }
+            else 
+            {
+                Console.WriteLine();
+            }
+        
+            private string Check(int month,int day)
+            {
+                string result;
+
+                if ((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10) || (month == 12))
+                    result = 
+            }
+
+        
+        }
+          
+         
+
+
+
+
+
+            
+
+         }
+
+    }
+

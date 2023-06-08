cb08dbb4c69d738b228936a20cbf1eb783ebc593
MSI\明坂　翔 <zhongx457@gmail.com>
2023-06-08 14:38:00 +0900

    練習問題５提出しました

---
+++ 練習問題５/練習問題５/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 練習問題５
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random random = new Random();
+            int answer = random.Next(101); // 0から100までのランダムな数を生成
+            int attempts = 0;
+            int guess = -1;
+
+            Console.WriteLine("0～100までの数字を当ててください");
+
+            while (guess != answer)
+            {
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out guess))
+                {
+                    Console.WriteLine("無効な入力です。再度入力してください。");
+                    continue;
+                }
+
+                if (guess < 0 || guess > 100)
+                {
+                    Console.WriteLine("範囲外の数字です。再度入力してください。");
+                    continue;
+                }
+
+                attempts++;
+
+                if (guess < answer)
+                {
+                    Console.WriteLine("もっと大きな数です");
+                }
+                else if (guess > answer)
+                {
+                    Console.WriteLine("もっと小さな数です");
+                }
+            }
+
+            Console.WriteLine("正解！ {0}回目で当たりました。", attempts);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 練習問題５－１/練習問題５－１/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 練習問題５_１
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***九九の表***");
+
+            // 横の数字を表示
+            Console.Write("   ");
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write($"{i,2} ");
+            }
+            Console.WriteLine();
+
+            // 表の区切り線を表示
+            Console.WriteLine("  ---------------------------");
+
+            // 九九の表を表示
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write($"{i,2}|");
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.Write($"{i * j,2} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("キーを押して終了します...");
+            Console.ReadKey();
+        }
+    }
+}

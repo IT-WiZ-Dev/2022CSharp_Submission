a918ba0564dcef36c7c3ab7acb35e1dcd38aaba5
DESKTOP-KANOU0S\大畑　拓真 <gabagabaaim3121@gmail.com>
2023-06-08 15:15:41 +0900

    九九の表と数あてゲームを提出

---
+++ 九九のひょう/九九のひょう/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int tableSize = 9; // 九九の表のサイズ
+
+        // タイトル行の表示
+        Console.Write("   ");
+        for (int i = 1; i <= tableSize; i++)
+        {
+            Console.Write($"{i,3} ");
+        }
+        Console.WriteLine();
+        Console.WriteLine(new string('-', (tableSize + 1) * 4)); // 区切り線の表示
+
+        // 九九の表の表示
+        for (int i = 1; i <= tableSize; i++)
+        {
+            Console.Write($"{i,2} |");
+            for (int j = 1; j <= tableSize; j++)
+            {
+                int result = i * j;
+                Console.Write($"{result,3} ");
+            }
+            Console.WriteLine();
+        }
+
+        // プログラムが終了する前に、コンソール画面を開いたままにするための入力待ち処理
+        Console.ReadLine();
+    }
+}
+
---
+++ 数あて/数あて/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int targetNumber = random.Next(101); // 0から100の範囲であたりの数を生成
+        int guessCount = 0; // ユーザーの回答回数
+        int minNumber = 0; // 現在のヒント範囲の最小値
+        int maxNumber = 100; // 現在のヒント範囲の最大値
+
+        Console.WriteLine("数あてゲームを開始します！");
+        Console.WriteLine($"0から100の間の数を当ててください。");
+
+        while (true)
+        {
+            Console.Write($"範囲: {minNumber} ～ {maxNumber} 、数を入力してください: ");
+            string input = Console.ReadLine();
+
+            int userNumber;
+            if (!int.TryParse(input, out userNumber))
+            {
+                Console.WriteLine("数値を入力してください。");
+                continue;
+            }
+
+            if (userNumber < minNumber || userNumber > maxNumber)
+            {
+                Console.WriteLine("範囲外の数です。正しい範囲内の数を入力してください。");
+                continue;
+            }
+
+            guessCount++;
+
+            if (userNumber == targetNumber)
+            {
+                Console.WriteLine($"おめでとうございます！{guessCount}回目で当たりました！");
+                break;
+            }
+            else if (userNumber < targetNumber)
+            {
+                Console.WriteLine("もっと大きい数です。");
+                minNumber = userNumber + 1;
+            }
+            else
+            {
+                Console.WriteLine("もっと小さい数です。");
+                maxNumber = userNumber - 1;
+            }
+        }
+
+        Console.WriteLine("ゲームを終了します。");
+        Console.ReadLine();
+    }
+}

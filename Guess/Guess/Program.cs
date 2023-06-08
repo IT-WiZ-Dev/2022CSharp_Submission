b4647a9bcced4ee87cb582b371c2908beab8fc09
MSI\ryupo <ryu.poke@icloud.com>
2023-06-08 16:00:24 +0900

    第５章練習問題を追加

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int minNumber = 1;
+        int maxNumber = 100;
+        Random random = new Random();
+        int targetNumber = random.Next(minNumber, maxNumber + 1);
+        int attempts = 0;
+        int guess = 0;
+
+        while (guess != targetNumber)
+        {
+            Console.Write($"{minNumber}から{maxNumber}の間の数字を当ててください。＞ ");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out guess))
+            {
+                Console.WriteLine("入力値が正しくありません。再入力してください。");
+                continue;
+            }
+
+            if (guess < minNumber || guess > maxNumber)
+            {
+                Console.WriteLine("入力値が正しくありません。再入力してください。");
+                continue;
+            }
+
+            attempts++;
+
+            if (guess < targetNumber)
+            {
+                Console.WriteLine("答はもっと大きいです。");
+                minNumber = guess + 1;
+                Console.WriteLine();
+            }
+            else if (guess > targetNumber)
+            {
+                Console.WriteLine("答はもっと小さいです。");
+                maxNumber = guess - 1;
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine($"おめでとう。{attempts}回目で当たりました。");
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   * * * 九九の表 * * *   ");
+
+            Console.Write("   |");
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write("{0,3}", i);
+            }
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 32));
+
+            for (int j = 1; j <= 9; j++)
+            {
+                Console.Write(j);
+                Console.Write("  |");
+
+                for (int k = 1; k <= 9; k++)
+                {
+                    Console.Write("{0,3}", j * k);
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+

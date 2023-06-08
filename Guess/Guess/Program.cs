4aaded896222acdbb4e5ed8687c28747b8e1bf9f
MSI\hurof <hirofumi022156@gmail.com>
2023-06-08 16:06:22 +0900

    第５章練習問題提出

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MinNumber = 0;
+            const int MaxNumber = 100;
+
+            Random random = new Random();
+            int targetNumber = random.Next(MinNumber, MaxNumber + 1);
+
+            int guessCount = 0;
+            bool isCorrect = false;
+            int previousGuess = -1;
+            int currentMin = MinNumber;
+            int currentMax = MaxNumber;
+
+            Console.WriteLine($"{MinNumber}から{MaxNumber}の間の数値を当ててください。");
+
+            while (!isCorrect)
+            {
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out int guess))
+                {
+                    Console.WriteLine("無効な入力です。整数を入力してください。");
+                    continue;
+                }
+
+                if (guess < currentMin || guess > currentMax)
+                {
+                    Console.WriteLine($"範囲外の数です。{currentMin}から{currentMax}の範囲で入力してください。");
+                    continue;
+                }
+
+                guessCount++;
+
+                if (guess == previousGuess)
+                {
+                    Console.WriteLine("同じ数を入力しました。別の数を入力してください。");
+                    continue;
+                }
+
+                if (guess < targetNumber)
+                {
+                    Console.WriteLine("答はもっと大きいです。");
+                    Console.WriteLine($"範囲は{guess + 1}から{currentMax}です。");
+                    currentMin = guess + 1;
+                }
+                else if (guess > targetNumber)
+                {
+                    Console.WriteLine("答はもっと小さいです。");
+                    Console.WriteLine($"範囲は{currentMin}から{guess - 1}です。");
+                    currentMax = guess - 1;
+                }
+                else
+                {
+                    isCorrect = true;
+                    Console.WriteLine($"おめでとう。{guessCount}回目で当たりました。");
+                }
+
+                previousGuess = guess;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Multiplication Table/Multiplication Table/Program.cs
@@ -0,0 +1,56 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+} */
+using System;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 左側の列を表示
+            Console.Write("   ");
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine();
+
+            // テーブルの区切り線を表示
+            Console.WriteLine("   ----------------------------------------------");
+
+            // 九九の表を表示するための2重ループ
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write($"{i,2}|");
+
+                for (int j = 1; j <= 9; j++)
+                {
+                    // 乗算の結果を計算し、出力する
+                    int result = i * j;
+                    Console.Write($"{result,3}");
+                }
+
+                // 1行終了後に改行する
+                Console.WriteLine();
+            }
+
+            // プログラムが終了する前に、コンソールウィンドウを開いたままにするために入力待ちを行う
+            Console.ReadLine();
+        }
+    }
+}
+

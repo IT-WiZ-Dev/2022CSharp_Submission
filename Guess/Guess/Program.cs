afcdf13d131fd2049178b6df09ba0a83108b650f
AmbaiHaruna <shun89218@gmail.com>
2023-06-08 14:47:47 +0900

    第5章練習問題を追加

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int answer = 32;
+            int result = 0;
+            int min = 0;
+            int max = 100;
+            int count = 0;
+
+            do
+            {
+                try
+                {
+                        Console.Write(min + "から" + max + "の間の数値を当ててください。＞");
+                        result = int.Parse(Console.ReadLine());
+
+                        if (result < min || max < result)
+                        {
+                            Console.WriteLine("入力値が正しくありません。再入力してください。");
+                            continue;
+                        }
+                        else if (result > answer)
+                        {
+                            count++;
+                            Console.WriteLine("答はもっと小さいです。");
+                            Console.Write('\n');
+                            max = result - 1;
+                            continue;
+                        }
+                        else if (result < answer)
+                        {
+                            count++;
+                            Console.WriteLine("答はもっと大きいです。");
+                            Console.Write('\n');
+                            min = result + 1;
+                            continue;
+                        }else if(result == answer)
+                    {
+                        count++;
+                        Console.WriteLine("おめでとう。" + count + "回目で当たりました。");
+                        Console.Write('\n');
+                        Console.Write("終了するには何かキーを押してください。");
+                        Console.Read();
+                        break;
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } while (true);
+        }
+    }
+}
---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   ＊＊＊九九の表＊＊＊");
+            Console.WriteLine("   |  1  2  3  4  5  6  7  8  9");
+            Console.WriteLine("-------------------------------");
+
+            for(int i = 1; i <= 9; i++)
+            {
+                Console.Write(i + "  |");
+                for(int j = 1; j <= 9; j++)
+                {
+                    Console.Write($"{i * j,3}");
+                }
+                Console.Write('\n');
+            }
+
+            Console.Write('\n');
+            Console.WriteLine("終了するには何かキーを押してください。");
+            Console.Read();
+        }
+    }
+}

fc6966e059f9238dd5589e468446433bedbc0ae6
DESKTOP-H5P1008\kaiza <kaizann1209@gmail.com>
2023-06-08 16:14:44 +0900

    第5章練習問題１つ追加

---
+++ Kuku/Kuku/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kuku
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("九九の表");
+
+            int kotae = 0;
+
+            for (int dan = 1; dan < 10; dan++)
+
+            {
+
+                for (int retu = 1; retu < 10; retu++)
+
+                {
+
+                    kotae = dan * retu;
+
+                    Console.Write("{0,3}",kotae);
+
+                }
+
+                Console.WriteLine();
+
+            }
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}

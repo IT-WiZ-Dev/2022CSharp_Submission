5e7dede13f1738146dfea820abd366291fc200db
LAPTOP-DP4UFD1R\MAHUNETAKUMI <mahunetakumi>
2023-06-08 15:50:06 +0900

    第5章練習問題提出

---
+++ ConsoleApp5-1/ConsoleApp5-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("   * * * 九九の表 * * *");
+            Console.Write("   |");
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine("\n--------------------------------");
+
+            for (int x = 1; x <= 9; x++)
+            {
+                Console.Write($"{x}  |");
+                for (int y = 1; y <= 9; y++)
+                {
+                    Console.Write($"{x * y,3}");
+                }
+                Console.Write('\n');
+            }
+
+            Console.WriteLine("\n終了するには何かキーを押してください。");
+            Console.Read();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            label6.Text = "";
+            label8.Text = "";
+
+                
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            int x;
+            double b;
+            double z;
+            const double Tax = 0.1;
+            a = int.Parse(textBox1.Text);
+            x = int.Parse(textBox2.Text);
+            b = a;
+            b *= (1 + Tax);
+            z = b / x;
+            b %= x;
+            a = (int)z;
+            x = (int)b;
+            label6.Text = a + "円";
+            label8.Text = x + "円";
+
+
+
+             
+            
+
+        }
+    }
+}

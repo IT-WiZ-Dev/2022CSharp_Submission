da9d3cf93151c0937dab709e7c0db439ded84714
MSI\owner <zeal22410134@fsgcl.jp>
2023-06-08 15:59:52 +0900

    第五章　練習問題

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int targetNumber = random.Next(1, 101);
+            int guess;
+            int attempts = 0;
+            int minRange = 0;
+            int maxRange = 100;
+
+            do
+            {
+                Console.Write(minRange + "から" + maxRange + "の間の数値を当ててください > ");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out guess))
+                {
+                    continue;
+                }
+
+                if (guess < minRange || guess > maxRange)
+                {
+                    Console.WriteLine("入力値が正しくありません。再入力してください。");
+                    continue;
+                }
+
+                attempts++;
+
+                if (guess < targetNumber)
+                {
+                    Console.WriteLine("答はもっと大きな数です。");
+                    minRange = guess;
+                }
+                else if (guess > targetNumber)
+                {
+                    Console.WriteLine("答はもっと小さな数です。");
+                    maxRange = guess;
+                }
+                else
+                {
+                    Console.WriteLine("おめでとう。" + attempts + "回目で当てました。");
+                    break;
+                }
+
+            } while (true);
+
+            Console.WriteLine("終了するには何かキーを押してください。");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("　＊＊＊九九の表＊＊＊");
+                Console.Write("  |");
+                for(int j = 1; j <= 9; j++)
+                {
+                    Console.Write($"{j,3}");
+                }
+                Console.Write("\n");
+            for(int h = 1;  h <=16; h++)
+            {
+                Console.Write("- ");
+            }
+            Console.Write("\n");
+            for(int t = 1; t <= 9; t++)
+            {
+                Console.Write(t + " |");
+                for(int y = 1; y <= 9; y++)
+                {
+                    int n = t * y;
+                    Console.Write($"{n,3}");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("終了するには何かキーを押してください");
+        }
+    }
+}

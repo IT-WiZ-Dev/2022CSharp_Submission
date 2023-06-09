41bccfb12a813f1f979b0d2cf5f5a5c556ba99a4
MSI\kariy <kariya2425@gmail.com>
2023-06-09 10:19:03 +0900

    練習問題第5章を提出

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int i = 7;//当たりの数
+            int min = 0, max = 100, num = 0;
+            Console.WriteLine("0から100の間の数値を当ててください。>");
+            int score = int.Parse(Console.ReadLine());//入力された文字列を整数型に変換
+            while(score != i)//入力した値がiと一致しなければ繰り返す
+            {
+                if(score > i&&score <= max)
+                {
+                    Console.WriteLine("答はもっと小さいです。");
+                    max = score - 1;
+                    num++;
+                    Console.WriteLine('\n');
+                    Console.WriteLine(min + "から" + max + "の間の数値を当ててください。>");
+                    score = int.Parse(Console.ReadLine());
+                }else if(score < i&&score >= min)
+                {
+                    Console.WriteLine("答はもっと大きいです。");
+                    min = score + 1;
+                    num++;
+                    Console.WriteLine('\n');
+                    Console.WriteLine(min + "から" + max + "の間の数値を当ててください。>");
+                    score = int.Parse(Console.ReadLine());
+                }else
+                {
+                    Console.WriteLine("入力値が正しくありません。再入力してください。");
+                    num++;
+                    score = int.Parse(Console.ReadLine());
+                }
+            }
+            num++;
+            Console.WriteLine("おめでとう。" + num + "回目で当たりました。");
+            Console.WriteLine('\n');
+            Console.WriteLine("終了するにはEnterキーを押してください。");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("＊＊＊九九の表＊＊＊");
+            for (int i = 1; i < 10; i++)
+            {
+                for(int j = 1; j < 10; j++)
+                    Console.Write("{0,3}", i*j);
+                Console.Write('\n');
+            }
+            Console.WriteLine("終了するにはEnterキーを押してください。");
+            Console.ReadLine();
+        }
+    }
+}

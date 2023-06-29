74f0a76329dbcd347e201253cc07cd4501e0bf4a
晃太朗 <zeal22410203@fsgcl.jp>
2023-06-29 13:48:16 +0900

    第6章練習問題提出

---
+++ MultiAlarm/MultiAlarm/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiAlarm
+{
+    public partial class Form1 : Form
+    {
+        private bool alarmSetFlag1 = false;
+        private bool alarmSetFlag2 = false;
+        private bool alarmSetFlag3 = false;
+
+        private int alarmhour1 = 0;
+        private int alarmhour2 = 0;
+        private int alarmhour3 = 0;
+
+        private int alarminute1 = 0;
+        private int alarminute2 = 0;
+        private int alarminute3 = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            labelTime.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            labelTime.Text = now.ToLongTimeString();
+            if (alarmSetFlag1 == true)
+            {
+                if (alarmhour1 == now.Hour &&
+                    alarminute1 == now.Minute)
+                {
+                    alarmSetFlag1 = false;
+                    MessageBox.Show("時間ですよ！","アラーム", MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    checkBox1.Checked = false;
+                }
+            }
+            if (alarmSetFlag2 == true)
+            {
+                if (alarmhour2 == now.Hour &&
+                    alarminute2 == now.Minute)
+                {
+                    alarmSetFlag2 = false;
+                    MessageBox.Show("時間ですよ！", "アラーム", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    checkBox2.Checked = false;
+                }
+            }
+            if (alarmSetFlag3 == true)
+            {
+                if (alarmhour3 == now.Hour &&
+                    alarminute3 == now.Minute)
+                {
+                    alarmSetFlag3 = false;
+                    MessageBox.Show("時間ですよ！", "アラーム", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    checkBox3.Checked = false;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            if(form2.ShowDialog() == DialogResult.OK)
+            {
+                alarmSetFlag1 = true;
+                alarmhour1 = form2.alarmHour;
+                alarminute1 = form2.alarmMinute;
+               
+                checkBox1.Text = alarmhour1.ToString("00") + "：" +
+                                 alarminute1.ToString("00");
+                checkBox1.Checked = true;
+            }
+            form2.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            if (form2.ShowDialog() == DialogResult.OK)
+            {
+                alarmSetFlag2 = true;
+                alarmhour2 = form2.alarmHour;
+                alarminute2 = form2.alarmMinute;
+               
+                checkBox2.Text = alarmhour2.ToString("00") + "：" +
+                                 alarminute2.ToString("00");
+                checkBox2.Checked = true;
+                form2.Dispose();
+            }
+   
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            if (form2.ShowDialog() == DialogResult.OK)
+            {
+                alarmSetFlag3 = true;
+                alarmhour3 = form2.alarmHour;
+                alarminute3 = form2.alarmMinute;
+               
+                checkBox3.Text = alarmhour3.ToString("00") + "：" +
+                                 alarminute3.ToString("00");
+                checkBox3.Checked = true;
+            }
+                
+            form2.Dispose();
+        }
+    }
+}
---
+++ MultiAlarm/MultiAlarm/Form2.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace MultiAlarm
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownHour = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownMinute = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinute)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownHour
+            // 
+            this.numericUpDownHour.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.numericUpDownHour.Location = new System.Drawing.Point(108, 43);
+            this.numericUpDownHour.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.numericUpDownHour.Name = "numericUpDownHour";
+            this.numericUpDownHour.Size = new System.Drawing.Size(169, 43);
+            this.numericUpDownHour.TabIndex = 0;
+            this.numericUpDownHour.ValueChanged += new System.EventHandler(this.NumericUpDownAlm_ValueChanged);
+            // 
+            // numericUpDownMinute
+            // 
+            this.numericUpDownMinute.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.numericUpDownMinute.Location = new System.Drawing.Point(108, 122);
+            this.numericUpDownMinute.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDownMinute.Name = "numericUpDownMinute";
+            this.numericUpDownMinute.Size = new System.Drawing.Size(169, 43);
+            this.numericUpDownMinute.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(108, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(108, 308);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 56);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "キャンセル";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(21, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 36);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "時";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(21, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 36);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "分";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 393);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDownMinute);
+            this.Controls.Add(this.numericUpDownHour);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinute)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownHour;
+        private System.Windows.Forms.NumericUpDown numericUpDownMinute;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}

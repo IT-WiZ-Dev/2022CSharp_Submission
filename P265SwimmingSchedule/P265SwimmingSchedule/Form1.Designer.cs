ff798268d98a7f9b233dd51768cb25d28299ce03
hatanakamasato <mata.maso.9060@gmail.com>
2023-06-29 15:41:40 +0900

    第７章練習問題

---
+++ P265SwimmingSchedule/P265SwimmingSchedule/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace P265SwimmingSchedule
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxCourse = new System.Windows.Forms.ListBox();
+            this.numericUpDownYear = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownMonth = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelDays = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelMoney = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownYear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxCourse
+            // 
+            this.listBoxCourse.FormattingEnabled = true;
+            this.listBoxCourse.ItemHeight = 18;
+            this.listBoxCourse.Location = new System.Drawing.Point(439, 59);
+            this.listBoxCourse.Name = "listBoxCourse";
+            this.listBoxCourse.Size = new System.Drawing.Size(206, 148);
+            this.listBoxCourse.TabIndex = 0;
+            // 
+            // numericUpDownYear
+            // 
+            this.numericUpDownYear.Location = new System.Drawing.Point(34, 59);
+            this.numericUpDownYear.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.numericUpDownYear.Name = "numericUpDownYear";
+            this.numericUpDownYear.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDownYear.TabIndex = 1;
+            // 
+            // numericUpDownMonth
+            // 
+            this.numericUpDownMonth.Location = new System.Drawing.Point(246, 59);
+            this.numericUpDownMonth.Name = "numericUpDownMonth";
+            this.numericUpDownMonth.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDownMonth.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(161, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "年";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(384, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "月";
+            // 
+            // labelDays
+            // 
+            this.labelDays.AutoSize = true;
+            this.labelDays.Location = new System.Drawing.Point(45, 153);
+            this.labelDays.Name = "labelDays";
+            this.labelDays.Size = new System.Drawing.Size(83, 18);
+            this.labelDays.TabIndex = 5;
+            this.labelDays.Text = "授業日　：";
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(45, 215);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(101, 18);
+            this.labelTime.TabIndex = 6;
+            this.labelTime.Text = "開始時間　：";
+            // 
+            // labelMoney
+            // 
+            this.labelMoney.AutoSize = true;
+            this.labelMoney.Location = new System.Drawing.Point(45, 284);
+            this.labelMoney.Name = "labelMoney";
+            this.labelMoney.Size = new System.Drawing.Size(83, 18);
+            this.labelMoney.TabIndex = 7;
+            this.labelMoney.Text = "授業料　：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(611, 33);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "表示";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelMoney);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.labelDays);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownMonth);
+            this.Controls.Add(this.numericUpDownYear);
+            this.Controls.Add(this.listBoxCourse);
+            this.Name = "Form1";
+            this.Text = "CSスイミングスクール";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownYear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxCourse;
+        private System.Windows.Forms.NumericUpDown numericUpDownYear;
+        private System.Windows.Forms.NumericUpDown numericUpDownMonth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelDays;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelMoney;
+        private System.Windows.Forms.Button button1;
+    }
+}
+

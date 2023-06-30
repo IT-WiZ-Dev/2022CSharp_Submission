878d8c1ac738ebde324cb56ccf45763613735ff1
DESKTOP-BJ636EH\hirak <hirakusan3@icloud.com>
2023-06-30 15:42:18 +0900

    ６章７章

---
+++ WindowsFormsApp_cha6_ren/WindowsFormsApp_cha6_ren/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace MultiAlarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.checkTime1 = new System.Windows.Forms.CheckBox();
+            this.checkTime2 = new System.Windows.Forms.CheckBox();
+            this.checkTime3 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(310, 50);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(67, 24);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "label5";
+            // 
+            // checkTime1
+            // 
+            this.checkTime1.AutoSize = true;
+            this.checkTime1.Location = new System.Drawing.Point(66, 141);
+            this.checkTime1.Name = "checkTime1";
+            this.checkTime1.Size = new System.Drawing.Size(151, 28);
+            this.checkTime1.TabIndex = 1;
+            this.checkTime1.Text = "checkBox4";
+            this.checkTime1.UseVisualStyleBackColor = true;
+            // 
+            // checkTime2
+            // 
+            this.checkTime2.AutoSize = true;
+            this.checkTime2.Location = new System.Drawing.Point(66, 235);
+            this.checkTime2.Name = "checkTime2";
+            this.checkTime2.Size = new System.Drawing.Size(151, 28);
+            this.checkTime2.TabIndex = 2;
+            this.checkTime2.Text = "checkBox5";
+            this.checkTime2.UseVisualStyleBackColor = true;
+            // 
+            // checkTime3
+            // 
+            this.checkTime3.AutoSize = true;
+            this.checkTime3.Location = new System.Drawing.Point(66, 349);
+            this.checkTime3.Name = "checkTime3";
+            this.checkTime3.Size = new System.Drawing.Size(151, 28);
+            this.checkTime3.TabIndex = 3;
+            this.checkTime3.Text = "checkBox6";
+            this.checkTime3.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(314, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 24);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(310, 236);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 24);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(310, 349);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 24);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "label8";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(615, 146);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(615, 240);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(615, 349);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 38);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(744, 498);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkTime3);
+            this.Controls.Add(this.checkTime2);
+            this.Controls.Add(this.checkTime1);
+            this.Controls.Add(this.labelTime);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.CheckBox checkTime1;
+        private System.Windows.Forms.CheckBox checkTime2;
+        private System.Windows.Forms.CheckBox checkTime3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsFormsApp_cha6_ren/WindowsFormsApp_cha6_ren/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiAlarm
+{
+    public partial class Form1 : Form
+    {
+        private bool alarmSetFlag1 = false;
+        private bool alarmSetFlag2 = false;
+        private bool alarmSetFlag3 = false;
+        private int alarmHour = 0;
+        private int alarmMinute = 0;
+        private int alarmSecond = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            labelTime.Text = DateTime.Now.ToLongTimeString();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            labelTime.Text = now.ToLongTimeString();
+
+            if (alarmSetFlag1 == true)
+            {
+                checkTime1.Checked = true;
+                if (alarmHour == now.Hour &&
+                   alarmMinute == now.Minute &&
+                   alarmSecond == now.Second)
+                {
+                    alarmSetFlag1 = false;
+                    MessageBox.Show("時間ですよ！", "アラーム",
+                           MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    checkTime1.Checked = false;
+                }
+            }
+            if (alarmSetFlag2 == true)
+            {
+                checkTime2.Checked = true;
+                if (alarmHour == now.Hour &&
+                   alarmMinute == now.Minute &&
+                   alarmSecond == now.Second)
+                {
+                    alarmSetFlag2 = false;
+                    MessageBox.Show("時間ですよ！", "アラーム",
+                           MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    checkTime2.Checked = false;
+                }
+            }
+            if (alarmSetFlag3 == true)
+            {
+                checkTime3.Checked = true;
+                if (alarmHour == now.Hour &&
+                   alarmMinute == now.Minute &&
+                   alarmSecond == now.Second)
+                {
+                    alarmSetFlag3 = false;
+                    MessageBox.Show("時間ですよ！", "アラーム",
+                           MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    checkTime3.Checked = false;
+                }
+            }
+
+        }
+
+        private void buttonset1_Click(object sender, EventArgs e)
+        {
+            Form2 formSet = new Form2();
+            formSet.ShowDialog();
+            formSet.Dispose();
+
+            alarmSetFlag1 = true;
+            alarmSetFlag3 = true;
+            alarmSetFlag2 = true;
+            alarmHour = formSet.alarmHour;
+            alarmMinute = formSet.alarmMinute;
+            alarmSecond = formSet.alarmSecond;
+            checkTime1.Text = alarmHour.ToString("00") + ":"
+                                + alarmMinute.ToString("00");
+        }
+
+        private void buttonset2_Click(object sender, EventArgs e)
+        {
+            Form2 formSet = new Form2();
+            formSet.ShowDialog();
+            formSet.Dispose();
+
+            alarmSetFlag1 = true;
+            alarmSetFlag2 = true;
+            alarmSetFlag3 = true;
+            alarmHour = formSet.alarmHour;
+            alarmMinute = formSet.alarmMinute;
+            alarmSecond = formSet.alarmSecond;
+            checkTime1.Text = alarmHour.ToString("00") + ":"
+                                + alarmMinute.ToString("00");
+        }
+
+        private void buttonset3_Click(object sender, EventArgs e)
+        {
+            Form2 formSet = new Form2();
+            formSet.ShowDialog();
+            formSet.Dispose();
+
+            alarmSetFlag1 = true;
+            alarmSetFlag2 = true;
+            alarmSetFlag3 = true;
+            alarmHour = formSet.alarmHour;
+            alarmMinute = formSet.alarmMinute;
+            alarmSecond = formSet.alarmSecond;
+            checkTime1.Text = alarmHour.ToString("00") + ":"
+                                + alarmMinute.ToString("00");
+        }
+    }
+}

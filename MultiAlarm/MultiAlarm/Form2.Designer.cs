15f01c58c4ab4e4cb2c25a1445dfe60e28183641
DESKTOP-VU75GP2\hashi <hashimoto2388@outlook.jp>
2023-07-06 13:55:03 +0900

    練習問題６提出

---
+++ MultiAlarm/MultiAlarm/Form2.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace MultiAlarm
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownHour = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownMnt = new System.Windows.Forms.NumericUpDown();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMnt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownHour
+            // 
+            this.numericUpDownHour.Location = new System.Drawing.Point(116, 46);
+            this.numericUpDownHour.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.numericUpDownHour.Name = "numericUpDownHour";
+            this.numericUpDownHour.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownHour.TabIndex = 0;
+            // 
+            // numericUpDownMnt
+            // 
+            this.numericUpDownMnt.Location = new System.Drawing.Point(116, 103);
+            this.numericUpDownMnt.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDownMnt.Name = "numericUpDownMnt";
+            this.numericUpDownMnt.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownMnt.TabIndex = 1;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(138, 159);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 2;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(138, 214);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "キャンセル";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.numericUpDownMnt);
+            this.Controls.Add(this.numericUpDownHour);
+            this.Name = "Form2";
+            this.Text = "時刻設定";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMnt)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownHour;
+        private System.Windows.Forms.NumericUpDown numericUpDownMnt;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ MultiAlarm/MultiAlarm/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiAlarm
+{
+    public partial class Form2 : Form
+    {
+        internal int alarmHour = 0;
+        internal int alarmMinute = 0;
+        internal int alarmSecond = 0;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            alarmHour = (int)numericUpDownHour.Value;
+            alarmMinute = (int)numericUpDownMnt.Value;
+            alarmSecond = 0;
+            
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.CancelButton = buttonCancel;
+        }
+    }
+}

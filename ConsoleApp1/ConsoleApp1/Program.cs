6169eb85998087fe0f5fc78cea16ecb69f726aca
MSI\Sun19 <yotarokageyama@gmail.com>
2023-07-06 14:53:03 +0900

    第５章練習問題1，2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("           ***九九の表***");
+        Console.WriteLine("   1  2  3  4  5  6  7  8  9");
+        Console.WriteLine("-----------------------------");
+
+
+        // 九九表を表示するための2重ループ
+        for (int i = 1; i <= 9; i++)
+        {
+            Console.Write(i + "|");
+            for (int j = 1; j <= 9; j++)
+            {
+                
+
+                // 九九表の計算と表示
+                int result = i * j;
+                Console.Write($"{result,2} "); // 数値を2桁で表示する
+
+                // 行の最後の列で改行する
+                if (j == 9)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        // プログラムの終了を待機する
+        Console.ReadLine();
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int targetNumber = random.Next(1, 101);
+        int guessCount = 0;
+        int guess;
+
+        Console.WriteLine("Up or Down ゲームを開始します！");
+        Console.WriteLine("1から100までの数を当ててください。");
+
+        do
+        {
+            Console.Write("予想した数を入力してください: ");
+            string input = Console.ReadLine();
+
+            // 入力値の検証
+            if (!int.TryParse(input, out guess))
+            {
+                Console.WriteLine("無効な入力です。数値を入力してください。");
+                continue;
+            }
+
+            if (guess < 1 || guess > 100)
+            {
+                Console.WriteLine("範囲外の数値です。1から100までの数を入力してください。");
+                continue;
+            }
+
+            guessCount++;
+
+            if (guess < targetNumber)
+            {
+                Console.WriteLine("Up!");
+            }
+            else if (guess > targetNumber)
+            {
+                Console.WriteLine("Down!");
+            }
+            else
+            {
+                Console.WriteLine($"正解です！{guessCount}回目で当たりました！");
+                break;
+            }
+        } while (true);
+
+        Console.WriteLine("ゲームを終了します。お疲れ様でした！");
+        Console.ReadLine();
+    }
+}

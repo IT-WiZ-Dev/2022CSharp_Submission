12e481ebffe6394d3f4ef1ce9102e3807a9e0d58
orikopg <ori.kou.1025@gmail.com>
2023-07-06 14:29:07 +0900

    第7章練習問題の提出

---
+++ SwimmingSchedule/SwimmingSchedule/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwimmingSchedule
+{
+    public partial class Form1 : Form
+    {
+        //コースのデータの配列
+        List<course> data;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            numericUpDown1.Value = now.Year;
+            numericUpDown2.Value = now.Month;
+
+            data = new List<course>();
+            data.Add(new course("ベビーコース", 1, 14, 1000));
+            data.Add(new course("幼児コース", 2, 10, 1000));
+            data.Add(new course("小学生コース", 3, 17, 800));
+            data.Add(new course("中学生コース", 4, 19, 800));
+            data.Add(new course("レディースコース", 5, 20, 1000));
+            data.Add(new course("一般コース", 6, 20, 1200));
+            data.Add(new course("家族コース", 0, 10, 1500));
+
+            foreach (course obj in data)
+            {
+                listBox1.Items.Add(obj.CourseName);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            string courseName = data[index].CourseName;
+            int dayOfWeek = data[index].DayOfWeek;
+
+            // 選択された年と月を取得
+            int year = (int)numericUpDown1.Value;
+            int month = (int)numericUpDown2.Value;
+
+            // 選択された年と月の最初の日を取得
+            DateTime startDate = new DateTime(year, month, 1);
+
+            // 選択された曜日と一致する日付を取得（月の最後の3日間を除外）
+            List<DateTime> matchingDates = course.GetMatchingDates(startDate, (DayOfWeek)dayOfWeek, 3);
+
+            // 結果を表示
+            label6.Text = string.Join(", ", matchingDates.Select(date => date.Day.ToString() + "日"));
+            label7.Text = data[index].StartTime.ToString() + "時";
+            int totalPrice = matchingDates.Count * data[index].Price;
+            label8.Text = totalPrice.ToString() + "円";
+        }
+    }
+}
---
+++ SwimmingSchedule/SwimmingSchedule/Swimmingcourse.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwimmingSchedule
+{
+    class course
+    {
+        // フィールド（クラス内の変数）
+        private string courseName;  // コースの名前
+        private int dayOfWeek;      // 曜日（1: 月曜日, 2: 火曜日, ...）
+        private int startTime;      // 開始時刻（24時間形式）
+        private int price;          // コースの価格
+
+        // コンストラクタ（クラスのインスタンスを初期化する特殊なメソッド）
+        public course(string name, int day, int time, int p)
+        {
+            this.courseName = name;
+            this.dayOfWeek = day;
+            this.startTime = time;
+            this.price = p;
+        }
+
+        // プロパティ（フィールドへのアクセスを制御するメソッド）
+        public string CourseName
+        {
+            get { return this.courseName; }   // コース名の取得
+            set { this.courseName = value; }  // コース名の設定
+        }
+        public int DayOfWeek
+        {
+            get { return this.dayOfWeek; }    // 曜日の取得
+            set { this.dayOfWeek = value; }   // 曜日の設定
+        }
+        public int StartTime
+        {
+            get { return this.startTime; }    // 開始時刻の取得
+            set { this.startTime = value; }   // 開始時刻の設定
+        }
+        public int Price
+        {
+            get { return this.price; }        // 価格の取得
+            set { this.price = value; }       // 価格の設定
+        }
+
+        public static List<DateTime> GetMatchingDates(DateTime startDate, DayOfWeek targetDayOfWeek, int excludeLastDays)
+        {
+            List<DateTime> matchingDates = new List<DateTime>();
+
+            // 最初の日から月末までの日付を調べて、曜日に一致する日をリストに追加
+            int year = startDate.Year;
+            int month = startDate.Month;
+            int daysInMonth = DateTime.DaysInMonth(year, month);
+
+            for (int day = 1; day <= daysInMonth - excludeLastDays; day++)
+            {
+                DateTime currentDate = new DateTime(year, month, day);
+                if (currentDate.DayOfWeek == targetDayOfWeek)
+                {
+                    matchingDates.Add(currentDate);
+                }
+            }
+            return matchingDates;
+        }
+
+    }
+}

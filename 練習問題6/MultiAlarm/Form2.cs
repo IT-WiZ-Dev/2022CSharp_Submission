604711915b6743e6611e3b04e579ad7dbb59c4dc
渡部　晴海 <noppon2118@icloud.com>
2023-07-14 09:54:16 +0900

    練習問題6提出

---
+++ 練習問題6/MultiAlarm/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiAlarm
+{
+    public partial class FormSet : Form
+    {
+        // フィールド
+        internal int AlarmHour = 0;       // アラーム時
+        internal int AlarmMinute = 0;     // アラーム分
+
+        public FormSet()
+        {
+            InitializeComponent();
+        }
+
+        //「設定ダイアログ」ロードのイベントハンドラ
+        private void FormSet_Load(object sender, EventArgs e)
+        {
+            // 現在時分の表示
+            numericUpDownHour.Value = DateTime.Now.Hour;
+            numericUpDownMinute.Value = DateTime.Now.Minute;
+        }
+
+        //「OKボタン」クリックのイベントハンドラ
+        private void ButtonOK_Click(object sender, EventArgs e)
+        {
+            // アラーム時刻の設定
+            AlarmHour = (int)numericUpDownHour.Value;
+            AlarmMinute = (int)numericUpDownMinute.Value;
+        }
+    }
+}

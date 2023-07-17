107f3c79bc00e4e56e83848c8c049b7b520189dd
二階堂祐太 <yuuta.nika.0701@gmail.com>
2023-07-17 13:37:57 +0900

    ７章練習問題

---
+++ SwimmingSchedule/SwimmingSchedule/Course.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwimmingSchedule
+{
+    class Course
+    {
+        private int week;                       // 曜日
+        private int startTime;                  // 開始時間
+        private int fee;                        // 1回分の授業料
+        private const int MinStartTime = 10;    // 最早開始時間
+        private const int MaxStartTime = 20;    // 最遅開始時間
+        //コンストラクタ
+        public Course(string name, int week, int startTime, int fee)
+        {
+            Name = name;
+            Week = week;
+            StartTime = startTime;
+            Fee = fee;
+
+        }
+        public string Name { get; private set; }
+
+        // 曜日（0:日　1:月　2:火　3:水　4:木　5:金　6:土）
+        public int Week
+        {
+            get { return week; }
+            set
+            {
+                if (value >= 0 && value <= 6)
+                    week = value;
+            }
+        }
+
+        // 開始時間
+        public int StartTime
+        {
+            get { return startTime; }
+            set
+            {
+                if (value >= MinStartTime && value <= MaxStartTime)
+                    startTime = value;
+            }
+        }
+
+        // 1回分の授業料
+        public int Fee
+        {
+            get { return fee; }
+            set
+            {
+                if (value >= 0)
+                    fee = value;
+            }
+        }
+
+       
+        public string SchoolDays(int year, int month)
+        {
+            int daysInMonth = DateTime.DaysInMonth(year, month);
+            string schoolDays = "";
+
+          
+            for (int day = 1; day <= daysInMonth - 3; day++)
+            {
+                DateTime dt = new DateTime(year, month, day);
+                if ((int)dt.DayOfWeek == Week)
+                    schoolDays += day + "日  ";
+            }
+
+            return schoolDays;
+        }
+
+        
+        public int SchoolFee(int year, int month)
+        {
+            int dayCount = 0;
+            int daysInMonth = DateTime.DaysInMonth(year, month);
+
+            
+            for (int day = 1; day <= daysInMonth - 3; day++)
+            {
+                DateTime dt = new DateTime(year, month, day);
+                if ((int)dt.DayOfWeek == Week)
+                    dayCount++;
+            }
+            return Fee * dayCount;
+        }
+    }
+}
---
+++ SwimmingSchedule/SwimmingSchedule/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwimmingSchedule
+{
+    public partial class Form1 : Form
+    {
+        private Course course1, course2, course3, course4, course5, course6, course7;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            course1 = new Course("ベビーコース", 1, 14, 1000);
+            course2 = new Course("幼児コース", 2, 10, 1000);
+            course3 = new Course("小学生コース", 3, 17, 800);
+            course4 = new Course("中学生コース", 4, 19, 800);
+            course5 = new Course("レディースコース", 5, 20, 1000);
+            course6 = new Course("一般コース", 6, 20, 1200);
+            course7 = new Course("家族コース", 0, 10, 1500);
+
+            numericUpDown1.Value = DateTime.Now.Year;
+            numericUpDown2.Value = DateTime.Now.Month;
+
+
+            listBox1.Items.Add(course1.Name);
+            listBox1.Items.Add(course2.Name);
+            listBox1.Items.Add(course3.Name);
+            listBox1.Items.Add(course4.Name);
+            listBox1.Items.Add(course5.Name);
+            listBox1.Items.Add(course6.Name);
+            listBox1.Items.Add(course7.Name);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+            int n = listBox1.SelectedIndex;
+            int year = (int)numericUpDown1.Value;
+            int month = (int)numericUpDown2.Value;
+
+          
+            switch (n)
+            {
+                case 0:
+                    label4.Text =course1.SchoolDays(year, month);
+                    label6.Text =course1.StartTime + "時";
+                    label8.Text = course1.SchoolFee(year, month) + "円";
+                    break;
+                case 1:
+                    label4.Text = course2.SchoolDays(year, month);
+                    label6.Text =  course2.StartTime + "時";
+                    label8.Text = course2.SchoolFee(year, month) + "円";
+                    break;
+                case 2:
+                    label4.Text = course3.SchoolDays(year, month);
+                    label6.Text = course3.StartTime + "時";
+                    label8.Text =  course3.SchoolFee(year, month) + "円";
+                    break;
+                case 3:
+                    label4.Text = course4.SchoolDays(year, month);
+                    label6.Text =  course4.StartTime + "時";
+                    label8.Text =  course4.SchoolFee(year, month) + "円";
+                    break;
+                case 4:
+                    label4.Text = course5.SchoolDays(year, month);
+                    label6.Text =  course5.StartTime + "時";
+                    label8.Text =  course5.SchoolFee(year, month) + "円";
+                    break;
+                case 5:
+                    label4.Text = course6.SchoolDays(year, month);
+                    label6.Text =  course6.StartTime + "時";
+                    label8.Text = course6.SchoolFee(year, month) + "円";
+                    break;
+                case 6:
+                    label4.Text =course7.SchoolDays(year, month);
+                    label6.Text =  course7.StartTime + "時";
+                    label8.Text = course7.SchoolFee(year, month) + "円";
+                    break;
+                default:
+                    label4.Text = "コースを選択してください";
+                    label6.Text = "";
+                    label8.Text = "";
+                    break;
+            }
+        }
+    }
+
+    
+}

8153bc1a465fe42417ae3dc4d2235d5a4596078a
annatokumoto <zeal22310046@fsgcl.jp>
2023-07-18 20:31:05 +0900

    第７章練習

---
+++ SwimmingSchedule 7練習/SwimmingSchedule/Course.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwimmingSchedule
+{
+    class Course
+    {
+        internal readonly int dayOfWeek;
+        private string courseName;
+        private int deyOfWeek;
+        private int startTime;
+        private int price;
+
+
+        //コンストラクタ
+        public Course(string name, int day, int time, int p)
+        {
+            this.courseName = name;
+            this.deyOfWeek = day;
+            this.startTime = time;
+            this.price = p;
+        }
+
+        //プロパティ
+        public string CourseName
+        {
+            set
+            {
+                this.courseName = value;
+            }
+            get
+            {
+                return this.courseName;
+            }
+        }
+
+    }
+}
---
+++ SwimmingSchedule 7練習/SwimmingSchedule/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace SwimmingSchedule
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.listBoxcourse = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelday = new System.Windows.Forms.Label();
+            this.labeltime = new System.Windows.Forms.Label();
+            this.labelen = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(50, 52);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2023,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(253, 52);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDown2.TabIndex = 1;
+            // 
+            // listBoxcourse
+            // 
+            this.listBoxcourse.FormattingEnabled = true;
+            this.listBoxcourse.ItemHeight = 18;
+            this.listBoxcourse.Location = new System.Drawing.Point(457, 52);
+            this.listBoxcourse.Name = "listBoxcourse";
+            this.listBoxcourse.Size = new System.Drawing.Size(160, 130);
+            this.listBoxcourse.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(588, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "表示";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "年";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(396, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "月";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "授業日 　  ：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "開始時間  ：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "授業料      ：";
+            // 
+            // labelday
+            // 
+            this.labelday.AutoSize = true;
+            this.labelday.Location = new System.Drawing.Point(166, 136);
+            this.labelday.Name = "labelday";
+            this.labelday.Size = new System.Drawing.Size(52, 18);
+            this.labelday.TabIndex = 9;
+            this.labelday.Text = "label6";
+            // 
+            // labeltime
+            // 
+            this.labeltime.AutoSize = true;
+            this.labeltime.Location = new System.Drawing.Point(166, 188);
+            this.labeltime.Name = "labeltime";
+            this.labeltime.Size = new System.Drawing.Size(52, 18);
+            this.labeltime.TabIndex = 10;
+            this.labeltime.Text = "label7";
+            // 
+            // labelen
+            // 
+            this.labelen.AutoSize = true;
+            this.labelen.Location = new System.Drawing.Point(166, 237);
+            this.labelen.Name = "labelen";
+            this.labelen.Size = new System.Drawing.Size(52, 18);
+            this.labelen.TabIndex = 11;
+            this.labelen.Text = "label8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 370);
+            this.Controls.Add(this.labelen);
+            this.Controls.Add(this.labeltime);
+            this.Controls.Add(this.labelday);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBoxcourse);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "CSスイミングスクール";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.ListBox listBoxcourse;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelday;
+        private System.Windows.Forms.Label labeltime;
+        private System.Windows.Forms.Label labelen;
+    }
+}
+

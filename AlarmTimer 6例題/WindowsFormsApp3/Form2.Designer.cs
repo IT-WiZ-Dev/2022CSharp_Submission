e1359e727cfc068d8fcf5920ddc0322f0d110154
annatokumoto <zeal22310046@fsgcl.jp>
2023-07-18 20:29:01 +0900

    第６章例題

---
+++ AlarmTimer 6例題/WindowsFormsApp3/Form2.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class FormSet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButtonAlarm = new System.Windows.Forms.RadioButton();
+            this.radioButtonTimer = new System.Windows.Forms.RadioButton();
+            this.numericUpDownAlmHour = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownAlmMnt = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownTimMnt = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownTimSec = new System.Windows.Forms.NumericUpDown();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAlmHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAlmMnt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTimMnt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTimSec)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButtonAlarm
+            // 
+            this.radioButtonAlarm.AutoSize = true;
+            this.radioButtonAlarm.ForeColor = System.Drawing.Color.White;
+            this.radioButtonAlarm.Location = new System.Drawing.Point(44, 37);
+            this.radioButtonAlarm.Name = "radioButtonAlarm";
+            this.radioButtonAlarm.Size = new System.Drawing.Size(89, 22);
+            this.radioButtonAlarm.TabIndex = 0;
+            this.radioButtonAlarm.TabStop = true;
+            this.radioButtonAlarm.Text = "アラーム";
+            this.radioButtonAlarm.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonTimer
+            // 
+            this.radioButtonTimer.AutoSize = true;
+            this.radioButtonTimer.ForeColor = System.Drawing.Color.White;
+            this.radioButtonTimer.Location = new System.Drawing.Point(259, 37);
+            this.radioButtonTimer.Name = "radioButtonTimer";
+            this.radioButtonTimer.Size = new System.Drawing.Size(87, 22);
+            this.radioButtonTimer.TabIndex = 1;
+            this.radioButtonTimer.TabStop = true;
+            this.radioButtonTimer.Text = "タイマー";
+            this.radioButtonTimer.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDownAlmHour
+            // 
+            this.numericUpDownAlmHour.Location = new System.Drawing.Point(44, 110);
+            this.numericUpDownAlmHour.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.numericUpDownAlmHour.Name = "numericUpDownAlmHour";
+            this.numericUpDownAlmHour.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDownAlmHour.TabIndex = 2;
+            this.numericUpDownAlmHour.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDownAlmHour.ValueChanged += new System.EventHandler(this.NumericUpDownAlm_ValueChanged);
+            // 
+            // numericUpDownAlmMnt
+            // 
+            this.numericUpDownAlmMnt.Location = new System.Drawing.Point(44, 182);
+            this.numericUpDownAlmMnt.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDownAlmMnt.Name = "numericUpDownAlmMnt";
+            this.numericUpDownAlmMnt.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDownAlmMnt.TabIndex = 3;
+            this.numericUpDownAlmMnt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDownAlmMnt.ValueChanged += new System.EventHandler(this.NumericUpDownAlm_ValueChanged);
+            // 
+            // numericUpDownTimMnt
+            // 
+            this.numericUpDownTimMnt.Location = new System.Drawing.Point(259, 110);
+            this.numericUpDownTimMnt.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDownTimMnt.Name = "numericUpDownTimMnt";
+            this.numericUpDownTimMnt.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDownTimMnt.TabIndex = 4;
+            this.numericUpDownTimMnt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDownTimMnt.VisibleChanged += new System.EventHandler(this.NumericUpDownTim_ValueChanged);
+            // 
+            // numericUpDownTimSec
+            // 
+            this.numericUpDownTimSec.Location = new System.Drawing.Point(259, 182);
+            this.numericUpDownTimSec.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDownTimSec.Name = "numericUpDownTimSec";
+            this.numericUpDownTimSec.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDownTimSec.TabIndex = 5;
+            this.numericUpDownTimSec.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDownTimSec.VisibleChanged += new System.EventHandler(this.NumericUpDownTim_ValueChanged);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.BackColor = System.Drawing.Color.MediumBlue;
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.ForeColor = System.Drawing.Color.White;
+            this.buttonOK.Location = new System.Drawing.Point(130, 252);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(101, 33);
+            this.buttonOK.TabIndex = 6;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = false;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.BackColor = System.Drawing.Color.MediumBlue;
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.ForeColor = System.Drawing.Color.White;
+            this.buttonCancel.Location = new System.Drawing.Point(237, 252);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(109, 33);
+            this.buttonCancel.TabIndex = 7;
+            this.buttonCancel.Text = "キャンセル";
+            this.buttonCancel.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 18);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "時";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "分";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(394, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "分";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(386, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 18);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "秒後";
+            // 
+            // FormSet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumBlue;
+            this.ClientSize = new System.Drawing.Size(520, 360);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.numericUpDownTimSec);
+            this.Controls.Add(this.numericUpDownTimMnt);
+            this.Controls.Add(this.numericUpDownAlmMnt);
+            this.Controls.Add(this.numericUpDownAlmHour);
+            this.Controls.Add(this.radioButtonTimer);
+            this.Controls.Add(this.radioButtonAlarm);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormSet";
+            this.Text = "設定";
+            this.Load += new System.EventHandler(this.FormSet_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAlmHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAlmMnt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTimMnt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTimSec)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonAlarm;
+        private System.Windows.Forms.RadioButton radioButtonTimer;
+        private System.Windows.Forms.NumericUpDown numericUpDownAlmHour;
+        private System.Windows.Forms.NumericUpDown numericUpDownAlmMnt;
+        private System.Windows.Forms.NumericUpDown numericUpDownTimMnt;
+        private System.Windows.Forms.NumericUpDown numericUpDownTimSec;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}

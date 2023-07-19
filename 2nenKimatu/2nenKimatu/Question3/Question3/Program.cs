f592c36dd9a2c7f40c6bae05becf66ef40688a5b
LAPTOP-F88O066O\mikiy <mikiyaaa3@gmail.com>
2023-07-19 15:58:39 +0900

    頑張りました期末

---
+++ 2nenKimatu/2nenKimatu/Question3/Question3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問3
+ * 下記の通りの参照渡しのメソッドを作成しなさい
+ * 
+ * メソッド名：AddNum
+ * 引数列：値渡しの整数型を２つ、参照渡しの整数型を１つ
+ * 戻り値：なし
+ * 処理：値渡しの整数を２つ足し算した結果を参照渡しの整数型へ代入するメソッド
+ * 
+ * 
+*/
+
+
+
+
+namespace Question3
+{
+    class Program
+    {
+        private static void AddNum(string[] args)
+        {
+            int a1;
+            int a2;
+
+            a1 = 1;
+        }
+       
+        {
+
+}
+    }
+}

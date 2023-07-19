a9cae4986a6a8d495b7e2b051f5ee9bc61f8f850
AzureAD\三橋桐(zeal22310120) <mituhasi1115@gmail.com>
2023-07-19 15:22:56 +0900

    期末問題提出

---
+++ Question2/Question2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問2
+ * 下記の指示通りクラスを作成しなさい。
+ * 
+ * クラス名：Student
+ * 〇メンバー変数
+ * 整数型　　：　studentId
+ * 文字列型　：　studentName
+ * 文字列型　：　otherInfo
+ * 
+ * ※各メンバーはプライベートで宣言し、getsetのプロパティを作成すること。
+ * 
+*/
+
+
+
+
+
+
+namespace Question2
+
+{
+
+    class Student
+
+    {
+
+        private int studentId;
+
+        private string studentName;
+
+        private string otherInfo;
+
+
+        public int StudentId
+
+        {
+
+            get { return studentId; }
+
+            set { studentId = value; }
+
+        }
+
+
+        public string StudentName
+
+        {
+
+            get { return studentName; }
+
+            set { studentName = value; }
+
+        }
+
+
+        public string OtherInfo
+
+        {
+
+            get { return otherInfo; }
+
+            set { otherInfo = value; }
+
+        }
+
+    }
+
+}
---
+++ Question4/Question4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問4
+ * Mainメソッドの中に配列arrが定義されている。
+ * foreach文を用いて配列arrの要素を全てコンソールに表示しなさい。 
+ * 
+*/
+
+
+namespace Question4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = new string[]{"orange","lemon","apple" };
+
+            foreach (string element in arr)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}

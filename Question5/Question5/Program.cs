066444465e32c9bc58d21a546f2184ea9145dede
MSI\hurof <hirofumi022156@gmail.com>
2023-07-19 15:54:53 +0900

    眞城博文　期末テスト第５問

---
+++ Question5/Question5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問5
+ * 下記でクラスBaseModelが定義されている。
+ * BaseModelを継承した派生クラスNewModelを作成してください。
+ * ※　エラーが出てる場合は不正解
+ * ※　クラスBaseModel内は変更してはいけない。
+*/
+
+
+namespace Question5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    // 継承元のBaseModel、変更してはいけません。
+    class BaseModel
+    {
+        public BaseModel(int num, string name)
+        {
+
+        }
+        public class NewModel : BaseModel
+        {
+
+        }
+    }
+
+
+}

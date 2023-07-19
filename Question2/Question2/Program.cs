70425b2a9c9e57f34b6b06986f7fe729a3eafc72
LAPTOP-DP4UFD1R\MAHUNETAKUMI <mahunetakumi>
2023-07-19 16:10:21 +0900

    期末課題

---
+++ Question2/Question2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問2
+ * 下記の指示通りクラスを作成しなさい。
+ * 
+ * クラス名：Student
+ * 〇メンバー変数
+ * 整数型　　：　studentId
+ * 文字列型　：　studentName
+ * 文字列型　：　otherInfo
+ * 
+ * ※各メンバーはプライベートで宣言し、getsetのプロパティを作成すること。
+ * 
+*/
+
+
+
+namespace Question2
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Question2/Question2/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+
+
+    class Student
+    {
+        private int studentId;
+        private string studentName;
+        private string otherInfo;
+        public Student(string studentName, string otherInfo, int studentId)
+        {
+            this.studentName = studentName;
+            this.otherInfo = otherInfo;
+            this.studentId = studentId;
+           
+        }
+        public int StudentId
+        {
+            set
+            {
+                this.studentId = value;
+            }
+            get
+            {
+                return this.studentId;
+            }
+        }
+        
+
+        
+    }
+}

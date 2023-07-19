0eee9b24c212c155ac7504cf5e84ef44708d6a66
LAPTOP-U2K6BUL5\ren <ren14420@icloud.com>
2023-07-19 15:08:57 +0900

    期末課題提出

---
+++ Question1/Question1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問1
+ * コンソール画面から入力された値を２つ受け取り、
+ * 足し算した結果を出力（コンソール画面に表示）しなさい。
+ * 
+ * 入力例：
+ * 15
+ * 20
+ * 
+ * 出力例：
+ * 35
+ * 
+*/
+
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1つ目の数字を入力してください。");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("2つ目の数字を入力してください。");
+            int y = int.Parse(Console.ReadLine());
+            int z = x + y;
+            Console.WriteLine($"結果は{z}です。");
+            Console.WriteLine("終了するには何かキーを押してください。");
+            Console.Read();
+        }
+    }
+}
---
+++ Question6/Question6/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCul_Click(object sender, EventArgs e)
+        {
+            //クラスの継承とかやりたかったけどあきらめました
+
+            int money = int.Parse(textBoxChange.Text);
+
+            //5000円札
+            int count = 0;
+            while (money >= 5000)
+            {
+                money -= 5000;
+                count += 1;
+            }
+            
+            label5000yen.Text = count.ToString();
+
+            //1000円札
+            count = 0;
+            while (money >= 1000)
+            {
+                money -= 1000;
+                count += 1;
+            }
+            label1000yen.Text = count.ToString();
+
+            //500円硬貨
+            count = 0;
+            while (money >= 500)
+            {
+                money -= 500;
+                count += 1;
+            }
+            label500yen.Text = count.ToString();
+
+            //100円硬貨
+            count = 0;
+            while (money >= 100)
+            {
+                money -= 100;
+                count += 1;
+            }
+            label100yen.Text = count.ToString();
+
+            //50円硬貨
+            count = 0;
+            while (money >= 50)
+            {
+                money -= 50;
+                count += 1;
+            }
+            label50yen.Text = count.ToString();
+
+            //10円硬貨
+            count = 0;
+            while (money >= 10)
+            {
+                money -= 10;
+                count += 1;
+            }
+            label10yen.Text = count.ToString();
+
+            //5円硬貨
+            count = 0;
+            while (money >= 5)
+            {
+                money -= 5;
+                count += 1;
+            }
+            label5yen.Text = count.ToString();
+
+            //1円硬貨
+            count = 0;
+            while (money >= 1)
+            {
+                money -= 1;
+                count += 1;
+            }
+            label1yen.Text = count.ToString();
+        }
+    }
+}

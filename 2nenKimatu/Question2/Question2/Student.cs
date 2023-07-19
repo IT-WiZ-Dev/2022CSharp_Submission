80ed2da65b78e552a2bd50bfd2f9e1033388f57c
MSI\ryupo <ryu.poke@icloud.com>
2023-07-19 16:13:10 +0900

    期末試験を提出

---
+++ 2nenKimatu/Question2/Question2/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    class Student
+    {
+        private string studentID;
+        private int studentName;
+        private int otherInfo;
+
+        public string StudentID
+        {
+            set
+            {
+                this.studentID = value;
+            }
+
+            get
+            {
+                return this.studentID;
+            }
+        }
+
+        public int StudentName
+        {
+            set
+            {
+                this.studentName = value;
+            }
+
+            get
+            {
+                return this.studentName;
+            }
+        }
+
+        public int OtherInfo
+        {
+            set
+            {
+                this.otherInfo = value;
+            }
+
+            get
+            {
+                return this.otherInfo;
+            }
+        }
+    }
+}

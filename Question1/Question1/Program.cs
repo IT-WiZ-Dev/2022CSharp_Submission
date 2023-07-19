bbbc4a396c458a863b16bfeb35acb7719a281f41
MSI\81907 <9rcf6f0u9dpainnapru@gmail.com>
2023-07-19 15:59:01 +0900

    期末試験提出

---
+++ Question1/Question1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問1
+ * コンソール画面から入力された値を２つ受け取り、
+ * 足し算した結果を出力（コンソール画面に表示）しなさい。
+ * 
+ * 入力例：
+ * 15
+ * 20
+ * 
+ * 出力例：
+ * 35
+ * 
+*/
+
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Console.WriteLine("1数字を入力してください");
+            String inputString = Console.ReadLine();
+
+            while (inputString != "quit")
+            {
+
+                x = Int32.Parse(inputString);
+
+                Console.WriteLine("2数字を入力してください");
+                inputString = Console.ReadLine();
+                y = Int32.Parse(inputString);
+
+                Console.WriteLine("合計は" + (x + y) + "です");
+
+                Console.WriteLine("1数字を入力してください");
+                inputString = Console.ReadLine();
+
+
+            }
+        }
+    }
+}
---
+++ Question2/Question2/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    class Student
+    {
+        private int studentId;
+        private string studentName;
+        private string otherInfo;
+
+        public int MyProperty { get; private set; }
+    }
+
+}

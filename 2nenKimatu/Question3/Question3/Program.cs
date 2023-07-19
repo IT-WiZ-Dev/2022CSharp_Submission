a077e4f5cf1d6d5f17f009347ba4d6844231fb62
Fukumoto0141 <fukukou1256@gmail.com>
2023-07-19 15:22:54 +0900

    期末

---
+++ 2nenKimatu/Question3/Question3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問3
+ * 下記の通りの参照渡しのメソッドを作成しなさい
+ * 
+ * メソッド名：AddNum
+ * 引数列：値渡しの整数型を２つ、参照渡しの整数型を１つ
+ * 戻り値：なし
+ * 処理：値渡しの整数を２つ足し算した結果を参照渡しの整数型へ代入するメソッド
+ * 
+ * 
+*/
+
+
+
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 10;
+            int c = 100;
+            int a = 0;
+            AddNum(ref a, b, c);
+            Console.Write(a);
+        }
+
+        public static void AddNum(ref int a, int b, int c) {
+            a = b + c;
+        }
+
+    }
+}
---
+++ 2nenKimatu/Question6/Question6/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxChange_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCul_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBoxChange.Text);
+            int num = a / 5000;
+            int c = a % 5000;
+            label5000yen.Text = num.ToString();
+            a = c;
+
+            num = a / 1000;
+            c = a % 1000;
+            label1000yen.Text = num.ToString();
+            a = c;
+
+            num = a / 500;
+            c = a % 500;
+            label500yen.Text = num.ToString();
+            a = c;
+
+            num = a / 100;
+            c = a % 100;
+            label100yen.Text = num.ToString();
+            a = c;
+
+            num = a / 50;
+            c = a % 50;
+            label50yen.Text = num.ToString();
+            a = c;
+
+            num = a / 10;
+            c = a % 10;
+            label10yen.Text = num.ToString();
+            a = c;
+
+            num = a / 5;
+            c = a % 5;
+            label5yen.Text = num.ToString();
+            a = c;
+
+            label1yen.Text = a.ToString();
+
+
+        }
+    }
+}

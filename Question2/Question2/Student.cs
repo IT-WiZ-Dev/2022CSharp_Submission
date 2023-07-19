33313a451f604fb7a56abbce71172e0351a1da53
MSI\owner <zeal22410134@fsgcl.jp>
2023-07-19 16:12:44 +0900

    C＃　期末課題　提出

---
+++ Question2/Question2/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    class Student
+    {
+        private int studentId { get; set; }
+        private string studentName { get; set; }
+        private string otherInfo { get; set; }
+        public int StudentId
+        {
+            get { return studentId; }
+            set{ studentId = value; }
+        }
+        public string StudentName
+        {
+            get { return studentName; }
+            set { studentName = value; }
+        }
+        public string OtherInfo
+        {
+            get { return otherInfo; }
+            set { otherInfo = value; }
+        }
+    }
+}
---
+++ Question6/Question6/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question6
+{
+    public partial class Form1 : Form
+    {
+        int oturi;
+        int gmai;
+        int smai;
+        int gimai;
+        int simai;
+        int gemai;
+        int semai;
+        int gmmai;
+        int smmai;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCul_Click(object sender, EventArgs e)
+        {
+            oturi = int .Parse(textBoxChange.Text);
+            if (oturi > 5000)
+            {
+                while (oturi > 5000)
+                {
+                    oturi = oturi - 5000;
+                    gmai += 1;
+                    if (oturi == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+                label5000yen.Text = gmai + "枚";
+            if (oturi > 1000)
+            {
+                while (oturi > 1000)
+                {
+                    oturi = oturi - 1000;
+                    smai += 1;
+                    if (oturi == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+                label1000yen.Text = smai + "枚";
+            if (oturi > 500)
+            {
+                while (oturi > 500)
+                {
+                    oturi = oturi - 500;
+                    gimai += 1;
+                    if (oturi == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+                label500yen.Text = gimai + "枚";
+            if (oturi > 100)
+            {
+                while (oturi > 100)
+                {
+                    oturi = oturi - 100;
+                    simai += 1;
+                    if (oturi == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+                label100yen.Text = simai + "枚";
+            if (oturi > 50)
+            {
+                while (oturi > 50)
+                {
+                    oturi = oturi - 50;
+                    gemai += 1;
+                    if (oturi == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+                label50yen.Text = gemai + "枚";
+        
+            if (oturi > 10)
+            {
+                while (oturi > 10)
+                {
+                    oturi = oturi - 10;
+                    semai += 1;
+                    if (oturi == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+                label10yen.Text = semai + "枚";
+            if (oturi > 5)
+            {
+                while (oturi > 5)
+                {
+                    oturi = oturi - 5;
+                    gmmai += 1;
+                    if (oturi == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+                label5yen.Text = gmmai + "枚";
+            if (oturi >= 1)
+            {
+                while (oturi > 1)
+                {
+                    oturi = oturi - 1;
+                    smmai += 1;
+                    if (oturi == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            label1yen.Text = smmai + "枚";
+        }
+    }
+}

dab3818df160915603ea01890b25afda1bfcc1a7
渡部　晴海 <noppon2118@icloud.com>
2023-07-19 15:24:09 +0900

    期末問題を提出

---
+++ Question1/Question1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問1
+ * コンソール画面から入力された値を２つ受け取り、
+ * 足し算した結果を出力（コンソール画面に表示）しなさい。
+ * 
+ * 入力例：
+ * 15
+ * 20
+ * 
+ * 出力例：
+ * 35
+ * 
+*/
+
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+           
+        {
+            Console.Write("");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("");
+            double num2 = double.Parse(Console.ReadLine());
+
+
+            double result = num1 + num2;
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Question3/Question3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問3
+ * 下記の通りの参照渡しのメソッドを作成しなさい
+ * 
+ * メソッド名：AddNum
+ * 引数列：値渡しの整数型を２つ、参照渡しの整数型を１つ
+ * 戻り値：なし
+ * 処理：値渡しの整数を２つ足し算した結果を参照渡しの整数型へ代入するメソッド
+ * 
+ * 
+*/
+
+
+
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 3;
+            int num2 = 6;
+            int result = 0;
+
+            AddNum(num1, num2, ref result);
+
+            Console.WriteLine("Result: " + result);
+            Console.ReadLine();
+        }
+
+        static void AddNum(int value1, int value2, ref int sum)
+        {
+            sum = value1 + value2;
+        }
+    }
+  }

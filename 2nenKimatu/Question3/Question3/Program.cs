6029d66619f473b342c7982d41083bcb2be530f9
ak03120 <ak03120@ymail.ne.jp>
2023-07-19 15:20:14 +0900

    2023年期末試験

---
+++ 2nenKimatu/Question3/Question3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問3
+ * 下記の通りの参照渡しのメソッドを作成しなさい
+ * 
+ * メソッド名：AddNum
+ * 引数列：値渡しの整数型を２つ、参照渡しの整数型を１つ
+ * 戻り値：なし
+ * 処理：値渡しの整数を２つ足し算した結果を参照渡しの整数型へ代入するメソッド
+ * 
+ * 
+*/
+
+
+
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //熊田晃大
+            int ans = 0;
+            AddNum(5, 7, ref ans);
+            Console.WriteLine(ans); // 12
+        }
+        static void AddNum(int val1, int val2, ref int val3)
+        {
+            val3 = val1 + val2;
+        }
+    }
+}
---
+++ 2nenKimatu/Question6/Question6/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //熊田晃大
+        private void buttonCul_Click(object sender, EventArgs e)
+        {
+            double yen = int.Parse(textBoxChange.Text);
+            label5000yen.Text = Math.Floor(yen / 5000).ToString("F0");
+            yen = yen % 5000;
+            label1000yen.Text = Math.Floor(yen / 1000).ToString("F0");
+            yen = yen % 1000;
+            label500yen.Text = Math.Floor(yen / 500).ToString("F0");
+            yen = yen % 500;
+            label100yen.Text = Math.Floor(yen / 100).ToString("F0");
+            yen = yen % 100;
+            label50yen.Text = Math.Floor(yen / 50).ToString("F0");
+            yen = yen % 50;
+            label10yen.Text = Math.Floor(yen / 10).ToString("F0");
+            yen = yen % 100;
+            label5yen.Text = Math.Floor(yen / 5).ToString("F0");
+            yen = yen % 5;
+            label1yen.Text = Math.Floor(yen / 1).ToString("F0");
+            yen = yen % 1;
+
+        }
+    }
+}

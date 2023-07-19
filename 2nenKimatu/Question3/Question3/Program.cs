00bc9ef4e53454c7f3619cb882f8fa701dea68fc
二階堂祐太 <yuuta.nika.0701@gmail.com>
2023-07-19 15:49:51 +0900

    期末課題

---
+++ 2nenKimatu/Question3/Question3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問3
+ * 下記の通りの参照渡しのメソッドを作成しなさい
+ * 
+ * メソッド名：AddNum
+ * 引数列：値渡しの整数型を２つ、参照渡しの整数型を１つ
+ * 戻り値：なし
+ * 処理：値渡しの整数を２つ足し算した結果を参照渡しの整数型へ代入するメソッド
+ * 
+ * 
+*/
+
+
+
+
+namespace Question3
+{
+    class Program
+    {
+     
+
+        static void Main(string[] args)
+        {
+            int AddNum = 10;
+            int a = 5;
+            Add(ref AddNum);
+            Add(ref a);
+            Console.WriteLine(AddNum+a);
+            Console.ReadLine();
+        }
+
+        static void Add(ref int refNum)
+        {
+            refNum = refNum + 10;
+        }
+    }
+}
---
+++ 2nenKimatu/Question6/Question6/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCul_Click(object sender, EventArgs e)
+        {
+            //変数と定数の宣言
+            int Change;//おつり
+
+            Change = int.Parse(textBoxChange.Text);
+
+            //5000yen
+
+            Change=
+
+
+        }
+    }
+}

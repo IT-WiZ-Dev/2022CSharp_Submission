5b2ac9b62df60b3caecbb9cb9451b6700fff002c
kaji yousuke <kaji09@outlook.jp>
2023-07-21 03:48:19 +0900

    第8章の練習問題提出

---
+++ Fishing/Fishing/Fish.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Fishing
+{
+    class Fish
+    {
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+        public int Speed { get; set; }
+        public bool IsFished { get; set; }
+
+        public PictureBox Pb { get; set; }
+
+        //コンストラクタ
+        public Fish(int posX, int posY, int speed, PictureBox pb)
+        {
+            PosX = posX;
+            PosY = posY;
+            Speed = speed;
+            Pb = pb;
+            IsFished = false;
+        }
+
+        public void Swim()
+        {
+            //int x = Pb.Location.X;
+            PosX += Speed;
+
+
+
+            Pb.Location = new Point(PosX,PosY) ;
+
+        }
+
+
+    }
+}
---
+++ Fishing/Fishing/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fishing
+{
+    public partial class Form1 : Form
+    {
+
+        Fish fish1;
+        Fish fish2;
+        Fish fish3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fish1 = new Fish(
+                pictureBox2.Location.X,
+                pictureBox2.Location.Y,
+                5,
+                pictureBox2
+                );
+
+            fish2 = new Fish(
+                pictureBox1.Location.X,
+                pictureBox1.Location.Y,
+                7,
+                pictureBox1
+                );
+
+            fish3 = new Fish(
+                pictureBox3.Location.X,
+                pictureBox3.Location.Y,
+                6,
+                pictureBox3
+                );
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            fish1.Swim();
+            fish2.Swim();
+            fish3.Swim();
+        }
+    }
+}

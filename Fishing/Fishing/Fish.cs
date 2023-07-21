b08eb066c333838c3e8033ffeb787613b3ac9992
MSI\joryu <joryu0725@au.com>
2023-07-21 16:53:36 +0900

    第８章練習問題提出

---
+++ Fishing/Fishing/Fish.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fishing
+{
+    class Fish
+    {
+        public int PosX { get; set; }
+
+        public int PosY { get; set; }
+
+        public int Speed { get; set; }
+
+        public bool IsFished { get; set; }
+
+        public PictureBox Pb {get;set;}
+        
+        //コンストラクタ
+        
+        public Fish(int posX,int posY,int speed,PictureBox pb)
+        {
+            PosX = posX;
+            PosY = posY;
+            Speed = speed;
+            Pb = pb;
+            IsFished = false;
+        }
+        //泳ぐメッソド
+
+        public void Swin()
+        {
+            int x = Pb.Location.X;
+            x += Speed;
+        }
+    }
+}
